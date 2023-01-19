76c938d09defe563cb9bdf918d56921ebeb9b3c4
Lim Junsuk <121474623+junsuk981102@users.noreply.github.com>
2023-01-19 10:19:37 +0900

    Update screen_Register.js

--- src/screen/screen_Register.js
+++ src/screen/screen_Register.js
@@ -31,28 +31,31 @@ const ScreenRegister = (props) => {
       console.log(error.message);
     }
   };
+
+
     return (
         <>
       <div>
         <h3>ScreenRegister 페이지입니다.</h3>
         아이디 :
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }}
-        />
+        <input placeholder="ID..."/>
+        <br/>
+        Email :
+        <input placeholder="Email..." onChange={(event) => {setRegisterEmail(event.target.value);}}/>
         <br/>
         비밀번호 :
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }}
-        />
+        <input placeholder="Password..." onChange={(event) => {setRegisterPassword(event.target.value);}} />
+        <br/>
+        비밀번호 확인 :
+        <input placeholder="Confirmed Password..."/>
+        <br/>
+        생년월일 :
+        <input placeholder="Birth date..."/>
+        <br/>
+        주소 :
+        <input placeholder="Address..."/>
         <br/>
         <button onClick={register}> 회원가입 </button>
-        
         <br/>
         <br/>
         <button onClick={() => handleClick("/screen_login")}>screen_login 페이지</button>
