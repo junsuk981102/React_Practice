ba154f6e38199252051389916f892aa5180f4978
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-02-22 10:40:31 +0900

    2023/02/22 update

--- src/screen/screen_Room_Make.js
+++ src/screen/screen_Room_Make.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import {useNavigate} from "react-router-dom";
 
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { dbService } from "../firebase-config";
 
 const ScreenRoomMake = (props) => {
