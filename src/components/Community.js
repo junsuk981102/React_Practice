d09f6cf2b48462e3fc2eb42826aafd5437d3c1aa
Kim, Sooyoung <83462874+kswim57@users.noreply.github.com>
2023-02-22 17:55:56 +0900

    Community 정보 Screen_Chat에 보내기

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const Community = ({ communityObj }) => {
+  const navi = useNavigate();
 
-        const navi = useNavigate();
-    
-        function handleClick() {
-            
-            navi(`/screen_Room_Info`, {state: {id: communityObj.id, name: communityObj.name, member:communityObj.member}});
-        }
+  function handleClick() {
+    navi(`/screen_room_info`, {
+      state: {
+        id: communityObj.id,
+        name: communityObj.name,
+        member: communityObj.member,
+      },
+    });
+  }
 
-    return (
+  function handleClick_chat() {
+    navi(`/screen_chat`, {
+      state: {
+        id: communityObj.id,
+        name: communityObj.name,
+        member: communityObj.member,
+      },
+    });
+  }
+
+  return (
     <div>
-        <h4>{communityObj.name}</h4>
-        <button onClick={() => handleClick()}>Move to Community</button>
-        
-        {/* <button>Delete Community</button>
+      <h4>{communityObj.name}</h4>
+      <button onClick={() => handleClick()}>Move to Community</button>
+      <br />
+      <button onClick={() => handleClick_chat()}>Move to ChatRoom</button>
+      <br />
+      {/* <button>Delete Community</button>
         <button>Edit Community name</button> */}
     </div>
-)
-}
+  );
+};
 export default Community;
--- src/screen/screen_Chat.js
+++ src/screen/screen_Chat.js
@@ -1,24 +1,28 @@
-import Chat from '../components/cpn_Chat'
-import '../chat.css'
-import {useNavigate} from "react-router-dom";
+import Chat from "../components/cpn_Chat";
+import "../chat.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function ScreenChat() {
+  const { state } = useLocation();
   const navi = useNavigate();
+  console.log(state.name);
 
   function handleClick(text) {
-      navi(`${text}`)
+    navi(`${text}`);
   }
 
   return (
     <>
-        <h3>ScreenChat 페이지입니다.</h3>
-        <button onClick={() => handleClick("/screen_room_list")}>뒤로가기</button>
-        <br/><br/>
-        <div className="ScreenChat">
-          <Chat/>
-        </div>
+      <h3>ScreenChat 페이지입니다.</h3>
+      <h4>{state.name}의 커뮤니티 입니다.</h4>
+      <button onClick={() => handleClick("/screen_room_list")}>뒤로가기</button>
+      <br />
+      <br />
+      <div className="ScreenChat">
+        <Chat />
+      </div>
     </>
-  )
+  );
 }
 
 export default ScreenChat;
