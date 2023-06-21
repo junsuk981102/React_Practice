afd131f2c91b1e89e8c316ef0ed913d1ad15d3b9
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-06-21 10:05:54 +0900

    update

--- src/screen/screen_Header.js
+++ src/screen/screen_Header.js
@@ -12,80 +12,16 @@ function Header(props) {
     return (
         <>
             <h1>헤더입니다.</h1>
-            {/*
-            <hr/>
             <div>
-                <h1>Link 방식</h1>
-                <Link to={"/"}>메인 페이지</Link>
+                <button onClick={() => handleClick("")}>홈</button>
                 {" "}
-                <Link to={"/screen_login"}>screen_login 페이지</Link>
+                <button onClick={() => handleClick("/screen_wallet_connect")}>지갑</button>
                 {" "}
-                <Link to={"/screen_register"}>screen_register 페이지</Link>
+                <button onClick={() => handleClick("/screen_room_list")}>커뮤니티 페이지</button>
                 {" "}
-                <Link to={"/screen_wallet_connect"}>screen_wallet_connect 페이지</Link>
+                <button onClick={() => handleClick("/screen_login")}>로그인</button>
                 {" "}
-                <Link to={"/screen_wallet_token"}>screen_wallet_token 페이지</Link>
-                {" "}
-                <Link to={"/screen_room_list"}>screen_room_list 페이지</Link>
-                {" "}
-                <Link to={"/screen_room_make"}>screen_room_make 페이지</Link>
-                {" "}
-                <Link to={"/screen_room_setting"}>screen_room_setting 페이지</Link>
-                {" "}
-                <Link to={"/screen_room_find"}>screen_room_find 페이지</Link>
-                {" "}
-                <Link to={"/screen_room_info"}>screen_room_info 페이지</Link>
-                {" "}
-                <Link to={"/screen_chat"}>screen_chat 페이지</Link>
-                {" "}
-                <Link to={"/screen_profile"}>screen_profile 페이지</Link>
-                {" "}
-                <Link to={"/screen_profile_info"}>screen_profile_info 페이지</Link>
-                {" "}
-                <Link to={"/screen_profile_user"}>screen_profile_user 페이지</Link>
-                {" "}
-                <Link to={"/screen_example"}>screen_example 페이지</Link>
-            </div>
-            <hr/>
-            <br/>
-            */}
-            <div>
-                {/*<h1>useNavigate 방식</h1>*/}
-                <button onClick={() => handleClick("")}>screen_main 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_login")}>screen_login 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_register")}>screen_register 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_wallet_connect")}>screen_wallet_connect 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_wallet_token")}>screen_wallet_token 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_room_list")}>screen_room_list 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_room_make")}>screen_room_make 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_room_setting")}>screen_room_setting 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_room_find")}>screen_room_find 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_room_info")}>screen_room_info 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_chat")}>screen_chat 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_profile")}>screen_profile 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_profile_info")}>screen_profile_info 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_profile_user")}>screen_profile_user 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_minting")}>screen_minting 페이지</button>
-                {" "}
-                {/*<button onClick={() => handleClick("/screen_my_animal")}>screen_my_animal 페이지</button>
-                {" "}*/}
-                <button onClick={() => handleClick("/screen_my_governance")}>screen_my_governance 페이지</button>
-                {" "}
-                <button onClick={() => handleClick("/screen_example")}>screen_example 페이지</button>
+                <button onClick={() => handleClick("/screen_register")}>회원가입</button>
                 {" "}
                 <button onClick={() => handleClick("/screen_test")}>screen_test 페이지</button>
             </div>
