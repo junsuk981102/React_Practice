48b04e6ff0a224f290552d0c51663728f046f36d
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-06-21 11:11:56 +0900

    Update screen_Main.js

--- src/screen/screen_Main.js
+++ src/screen/screen_Main.js
@@ -10,7 +10,7 @@ const ScreenMain = (props) => {
     
     return (
         <>
-            <img src="image/stot_main.png" alt="logo"/>\
+            <img src="image/stot_main.png" alt="logo"/>
             <h3>새로운 스타트업 찾기</h3>
             <h3>투자중인 스타트업</h3>
         </>
