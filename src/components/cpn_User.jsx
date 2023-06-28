13b237552d31db931c878a0f588ae185a76ccc8e
Kim, Sooyoung <83462874+kswim57@users.noreply.github.com>
2023-06-28 16:55:23 +0900

    Login / User 수정
    로그인 후 User Profile image default 등록, UI 수정

--- src/components/cpn_User.jsx
+++ src/components/cpn_User.jsx
@@ -1,23 +1,51 @@
-import Login from './cpn_Login'
-import Logout from './cpn_Logout';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase-config';
+import Login from "./cpn_Login";
+import Logout from "./cpn_Logout";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase-config";
 
 const User = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const photo = user ? user.photoURL : "/userImage.png";
-    const name = user ? user.displayName : "Name User";
-    return ( 
-        <div className='right-side'>
-            <h1><i className="fa-solid fa-cat"></i></h1>
-            <article className='card-user'>
-                <img src={photo} alt="user default" />
-                <p>{name}</p>
-                { user ? <Logout/> : <Login/> }
-            </article>
-        </div>
-    );
-}
- 
+  const defaultProfileImage = "/image/user.png"; // 기본 디폴트 이미지 경로
+  const photo = user?.photoURL || defaultProfileImage;
+  const name = user?.displayName;
+
+  return (
+    <div
+      style={{
+        margin: "15px",
+      }}
+    >
+      <article
+        style={{
+          border: "1px solid #d4d3d3",
+          borderRadius: "25px",
+          backgroundColor: "#f5f8fc",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "20px",
+          width: "30vw",
+        }}
+      >
+        <img
+          src={photo}
+          alt="User Profile"
+          style={{
+            width: "90px",
+            borderRadius: "50%",
+            display: "block",
+          }}
+          onError={(e) => {
+            e.target.src = defaultProfileImage;
+          }}
+        />
+        <p style={{ fontWeight: "500" }}>{name || "Please Sign in"}</p>
+        {user ? <Logout /> : <Login />}
+      </article>
+    </div>
+  );
+};
+
 export default User;
--- src/screen/screen_Login.js
+++ src/screen/screen_Login.js
@@ -6,13 +6,21 @@ function ScreenLogin() {
     <>
       <div
         style={{
+          display: "flex",
           backgroundColor: "#E5F2F2",
-          minHeight: "100vh",
           borderTop: "1px solid #00A29D",
+          justifyContent: "center",
+          height: "100vh",
         }}
       >
-        <h3>ScreenLogin 페이지입니다.</h3>
-        <div className="ScreenProfileUser">
+        {/* <h3>ScreenLogin 페이지입니다.</h3> */}
+        <div
+          style={{
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+          }}
+        >
           <User />
         </div>
       </div>
