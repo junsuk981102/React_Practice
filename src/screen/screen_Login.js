7f2dd5b542e9833c7517b90372300fed20531f02
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-06-29 14:51:58 +0900

    update

--- src/screen/screen_Login.js
+++ src/screen/screen_Login.js
@@ -1,18 +1,31 @@
-import User from '../components/cpn_User'
-import '../chat.css'
+import User from "../components/cpn_User";
+import "../chat.css";
 
 function ScreenLogin() {
   return (
     <>
-      <div style={{ backgroundColor: "#E5F2F2", minHeight: "100vh", borderTop: "1px solid #00A29D" }}>
-        <h3>ScreenLogin 페이지입니다.</h3>
-          <div className="ScreenProfileUser">
-            <User/>
-          </div>
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "#E5F2F2",
+          borderTop: "1px solid #00A29D",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        {/* <h3>ScreenLogin 페이지입니다.</h3> */}
+        <div
+          style={{
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+          }}
+        >
+          <User />
+        </div>
       </div>
-
     </>
-  )
+  );
 }
 
 export default ScreenLogin;
