a18c1be5f7d6cb4249254b43a8a6e9c2b8d1ffe2
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-06-29 11:54:39 +0900

    Update screen_Room_Info.js

--- src/screen/screen_Room_Info.js
+++ src/screen/screen_Room_Info.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import {useNavigate, useLocation} from "react-router-dom";
 
 const ScreenRoomInfo = (props) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
+    useEffect(() => {
+      setWindowWidth(window.innerWidth);
+    }, []);
+
     const { state } = useLocation();
     console.log(state.name);
 
@@ -11,32 +29,47 @@ const ScreenRoomInfo = (props) => {
         navi(`${text}`)
     }
 
+
     return (
         <>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    minHeight: "100vh",
-                    backgroundColor: "#E5F2F2",
-                    borderTop: "1px solid #00A29D"
-                }}
-            >   
-                <h3>ScreenRoomInfo 페이지입니다.</h3>
-                <h4>커뮤니티 이름: {state.com_name}</h4>
-                <h4>커뮤니티 범주: {state.com_category}</h4>
-                <h4>커뮤니티 정보: {state.com_info}</h4>
-                <h4>커뮤니티 목표 투자금액: {state.com_total_investment}원</h4>
-                <h4>커뮤니티 현재 투자금액: {state.com_now_investment}원</h4>
-                <h4>커뮤니티 티켓 가격: {state.com_ticket_price}원</h4>
-                <h4>커뮤니티 티켓 최대 개수: {state.com_ticket_max}개</h4>
-                <h4>커뮤니티 참여 인원 수: {state.com_member}명</h4>
-                <button onClick={() => handleClick("/screen_room_find")}>뒤로가기</button>
-                <br/><br/>
-                <button onClick={() => handleClick("/screen_chat")}>screen_chat 페이지</button>
+            <div style={{ backgroundColor: "#E5F2F2", minHeight: "100vh", borderTop: "1px solid #00A29D" }}>
+                <div
+                style={{ paddingLeft: `${windowWidth > 1700 ? '500px' : '50px'}`, paddingRight: `${windowWidth > 1700 ? '500px' : '10px'}` }}
+                >
+                    <h3 style={{ margin: "0", paddingTop: "50px", marginBottom: "30px", fontSize: "24px", fontWeight: "bold" }}>커뮤니티 소개</h3>
+                    <div className="test"
+                        style={{
+                        width: "1000px",height: "1000px",backgroundColor: "white",border: "3px solid #00A29D",
+                        padding: "30px",display: "flex",position: "relative",borderRadius: "15px",
+                        }}
+                    >
+                    <img
+                    src="../image/animal/2.png"
+                    alt="Animal"
+                    style={{
+                        width: "200px",
+                        height: "200px",
+                        objectFit: "cover",
+                        borderRadius: "50%"
+                    }}
+                    />
+                    <div style={{ padding: "20px"}}>
+                        <h3>ScreenRoomInfo 페이지입니다.</h3>
+                        <h4>커뮤니티 이름: {state.com_name}</h4>
+                        <h4>커뮤니티 범주: {state.com_category}</h4>
+                        <h4>커뮤니티 정보: {state.com_info}</h4>
+                        <h4>커뮤니티 목표 투자금액: {state.com_total_investment}원</h4>
+                        <h4>커뮤니티 현재 투자금액: {state.com_now_investment}원</h4>
+                        <h4>커뮤니티 티켓 가격: {state.com_ticket_price}원</h4>
+                        <h4>커뮤니티 티켓 최대 개수: {state.com_ticket_max}개</h4>
+                        <h4>커뮤니티 참여 인원 수: {state.com_member}명</h4>
+                        <button onClick={() => handleClick("/screen_room_find")}>뒤로가기</button>
+                        <br/><br/>
+                        <button onClick={() => handleClick("/screen_chat")}>screen_chat 페이지</button>
+                    </div>
+                </div>
+                </div>
             </div>
-
         </>
     );
 }
