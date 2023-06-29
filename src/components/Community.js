272349e80e0594790545098896be0967f3e27ef1
Kim, Sooyoung <83462874+kswim57@users.noreply.github.com>
2023-06-29 17:09:07 +0900

    screen_Room_Info 와 screen_Chat 연결 오류 수정
    기존에 페이지가 넘어가도 Community의 정보를 못 들고가서 페이지에 아무것도 뜨지 않던 에러 해결.

--- src/components/Community.js
+++ src/components/Community.js
@@ -8,14 +8,14 @@ const Community = ({ communityObj }) => {
     navi(`/screen_room_info`, {
       state: {
         id: communityObj.id,
-        com_name : communityObj.com_name,
-        com_category : communityObj.com_category,
-        com_info : communityObj.com_info,
-        com_total_investment : communityObj.com_total_investment,
-        com_now_investment : communityObj.com_now_investment,
-        com_ticket_price : communityObj.com_ticket_price,
-        com_ticket_max : communityObj.com_ticket_max,
-        com_member : communityObj.com_member,
+        com_name: communityObj.com_name,
+        com_category: communityObj.com_category,
+        com_info: communityObj.com_info,
+        com_total_investment: communityObj.com_total_investment,
+        com_now_investment: communityObj.com_now_investment,
+        com_ticket_price: communityObj.com_ticket_price,
+        com_ticket_max: communityObj.com_ticket_max,
+        com_member: communityObj.com_member,
       },
     });
   }
@@ -24,31 +24,84 @@ const Community = ({ communityObj }) => {
     navi(`/screen_chat`, {
       state: {
         id: communityObj.id,
-        com_name : communityObj.com_name,
-        com_category : communityObj.com_category,
-        com_info : communityObj.com_info,
-        com_total_investment : communityObj.com_total_investment,
-        com_now_investment : communityObj.com_now_investment,
-        com_ticket_price : communityObj.com_ticket_price,
-        com_ticket_max : communityObj.com_ticket_max,
-        com_member : communityObj.com_member,
+        com_name: communityObj.com_name,
+        com_category: communityObj.com_category,
+        com_info: communityObj.com_info,
+        com_total_investment: communityObj.com_total_investment,
+        com_now_investment: communityObj.com_now_investment,
+        com_ticket_price: communityObj.com_ticket_price,
+        com_ticket_max: communityObj.com_ticket_max,
+        com_member: communityObj.com_member,
       },
     });
   }
 
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
-    <h4 style={{ position: "absolute", top: 10, left: 10, fontSize: "28px", fontWeight: "bold" }}>{communityObj.com_name}</h4>
-    <h4 style={{ position: "absolute", top: 50, left: 12, fontSize: "12px", fontWeight: "bold", color: "#00A29D" }}>{communityObj.com_category}</h4>
-    <button style={{ position: "absolute", top: 15, right: 20, fontSize: "10px",fontWeight: "bold" }} onClick={() => handleClick()}>
-    자세히 보기 {'>'}
-    </button>
-    <button style={{ position: "absolute",borderRadius: "10px",color: "white",backgroundColor: "#00A29D",padding: "4px 8px",bottom: 15, right: 20, fontSize: "18px", fontWeight: "bold" }} onClick={() => handleClick_chat()}>
-    채팅방 입장 {'>'}
-    </button>
-    <h4 style={{ position: "absolute", bottom: 20, left: 12, fontSize: "16px", color: "#00A29D", fontWeight: "bold" }}>현재 : {communityObj.com_member}명</h4>
+      <h4
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          fontSize: "28px",
+          fontWeight: "bold",
+        }}
+      >
+        {communityObj.com_name}
+      </h4>
+      <h4
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 12,
+          fontSize: "12px",
+          fontWeight: "bold",
+          color: "#00A29D",
+        }}
+      >
+        {communityObj.com_category}
+      </h4>
+      <button
+        style={{
+          position: "absolute",
+          top: 15,
+          right: 20,
+          fontSize: "10px",
+          fontWeight: "bold",
+        }}
+        onClick={() => handleClick()}
+      >
+        자세히 보기 {">"}
+      </button>
+      <button
+        style={{
+          position: "absolute",
+          borderRadius: "10px",
+          color: "white",
+          backgroundColor: "#00A29D",
+          padding: "4px 8px",
+          bottom: 15,
+          right: 20,
+          fontSize: "18px",
+          fontWeight: "bold",
+        }}
+        onClick={() => handleClick_chat()}
+      >
+        채팅방 입장 {">"}
+      </button>
+      <h4
+        style={{
+          position: "absolute",
+          bottom: 20,
+          left: 12,
+          fontSize: "16px",
+          color: "#00A29D",
+          fontWeight: "bold",
+        }}
+      >
+        현재 : {communityObj.com_member}명
+      </h4>
     </div>
   );
 };
 export default Community;
-
--- src/screen/screen_Room_Info.js
+++ src/screen/screen_Room_Info.js
@@ -1,32 +1,47 @@
-import React from 'react';
-import {useNavigate, useLocation} from "react-router-dom";
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const ScreenRoomInfo = (props) => {
-    const { state } = useLocation();
-    console.log(state.name);
+  const { state } = useLocation();
+  const navi = useNavigate();
 
-    const navi = useNavigate();
+  function handleClick(text) {
+    navi(`${text}`);
+  }
 
-    function handleClick(text) {
-        navi(`${text}`)
-    }
+  function handleClick_chat() {
+    navi("/screen_chat", {
+      state: {
+        id: state.id,
+        com_name: state.com_name,
+        com_category: state.com_category,
+        com_info: state.com_info,
+        com_total_investment: state.com_total_investment,
+        com_now_investment: state.com_now_investment,
+        com_ticket_price: state.com_ticket_price,
+        com_ticket_max: state.com_ticket_max,
+        com_member: state.com_member,
+      },
+    });
+  }
 
-    return (
-        <>
-            <h3>ScreenRoomInfo 페이지입니다.</h3>
-            <h4>커뮤니티 이름: {state.com_name}</h4>
-            <h4>커뮤니티 범주: {state.com_category}</h4>
-            <h4>커뮤니티 정보: {state.com_info}</h4>
-            <h4>커뮤니티 목표 투자금액: {state.com_total_investment}원</h4>
-            <h4>커뮤니티 현재 투자금액: {state.com_now_investment}원</h4>
-            <h4>커뮤니티 티켓 가격: {state.com_ticket_price}원</h4>
-            <h4>커뮤니티 티켓 최대 개수: {state.com_ticket_max}개</h4>
-            <h4>커뮤니티 참여 인원 수: {state.com_member}명</h4>
-            <button onClick={() => handleClick("/screen_room_find")}>뒤로가기</button>
-            <br/><br/>
-            <button onClick={() => handleClick("/screen_chat")}>screen_chat 페이지</button>
-        </>
-    );
-}
+  return (
+    <>
+      <h3>ScreenRoomInfo 페이지입니다.</h3>
+      <h4>커뮤니티 이름: {state.com_name}</h4>
+      <h4>커뮤니티 범주: {state.com_category}</h4>
+      <h4>커뮤니티 정보: {state.com_info}</h4>
+      <h4>커뮤니티 목표 투자금액: {state.com_total_investment}원</h4>
+      <h4>커뮤니티 현재 투자금액: {state.com_now_investment}원</h4>
+      <h4>커뮤니티 티켓 가격: {state.com_ticket_price}원</h4>
+      <h4>커뮤니티 티켓 최대 개수: {state.com_ticket_max}개</h4>
+      <h4>커뮤니티 참여 인원 수: {state.com_member}명</h4>
+      <button onClick={() => handleClick("/screen_room_find")}>뒤로가기</button>
+      <br />
+      <br />
+      <button onClick={() => handleClick_chat()}>screen_chat 페이지</button>
+    </>
+  );
+};
 
 export default ScreenRoomInfo;
