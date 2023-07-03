aa2c1ca39fd9d03e41f939e26b7858c6c45b1814
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-03 14:32:52 +0900

    Update screen_Room_Info.js

--- src/screen/screen_Room_Info.js
+++ src/screen/screen_Room_Info.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+
 const ScreenRoomInfo = (props) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   useEffect(() => {
