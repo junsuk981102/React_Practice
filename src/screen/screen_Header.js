e6b6938ea3f7b938b9119efc123301b02a35b21a
kswim57 <kswim8914@gmail.com>
2023-07-04 09:20:53 +0900

    로그인 상태에 따른 헤더 구성 변경
    로그인/회원가입 에서 로그인 된 이후에는 로그아웃으로 변경

--- src/screen/screen_Header.js
+++ src/screen/screen_Header.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+//로그인 상태를 감지하기 위한 firebase auth
+import { auth } from "../firebase-config";
 
 function Header(props) {
   const navi = useNavigate();
   const [activeButton, setActiveButton] = useState("");
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 추가된 상태
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,10 +25,72 @@ function Header(props) {
     setWindowWidth(window.innerWidth);
   }, []);
 
+  // 추가된 useEffect
+  useEffect(() => {
+    // 로그인 상태 변경 감지
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setIsLoggedIn(!!user); // 로그인 상태에 따라 true 또는 false 설정
+    });
+
+    return () => {
+      unsubscribe(); // cleanup 함수에서 구독 해제
+    };
+  }, []);
+
   function handleClick(text) {
     setActiveButton(text);
     navi(text);
   }
+
+  // 수정된 renderButton 함수
+  const renderButton = () => {
+    if (isLoggedIn) {
+      return (
+        <button
+          onClick={() => {
+            // 로그아웃 처리
+            auth.signOut().then(() => {
+              // 로그아웃 후 처리
+              console.log("로그아웃 성공");
+            });
+          }}
+          style={{
+            marginRight: "20px",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          로그아웃
+        </button>
+      );
+    } else {
+      return (
+        <>
+          <button
+            onClick={() => handleClick("/screen_Login")}
+            style={{
+              marginRight: "20px",
+              fontSize: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            로그인
+          </button>
+          <button
+            onClick={() => handleClick("/screen_register")}
+            style={{
+              marginRight: `${windowWidth > 1700 ? "500px" : "10px"}`,
+              fontSize: "20px",
+              fontWeight: "bold",
+              color: "#00A29D",
+            }}
+          >
+            회원가입
+          </button>
+        </>
+      );
+    }
+  };
 
   return (
     <>
@@ -40,7 +105,7 @@ function Header(props) {
         <div style={{ display: "flex", alignItems: "center" }}>
           <div
             style={{
-              marginRight: `${windowWidth > 1700 ? "32px" : "10px"}`,
+              marginRight: `${windowWidth > 1700 ? "32px" : "40px"}`,
               marginLeft: `${windowWidth > 1700 ? "485px" : "35px"}`,
             }}
           >
@@ -86,29 +151,7 @@ function Header(props) {
             커뮤니티
           </button>
         </div>
-        <div>
-          <button
-            onClick={() => handleClick("/screen_Login")}
-            style={{
-              marginRight: "20px",
-              fontSize: "20px",
-              fontWeight: "bold",
-            }}
-          >
-            로그인
-          </button>
-          <button
-            onClick={() => handleClick("/screen_register")}
-            style={{
-              marginRight: `${windowWidth > 1700 ? "500px" : "10px"}`,
-              fontSize: "20px",
-              fontWeight: "bold",
-              color: "#00A29D",
-            }}
-          >
-            회원가입
-          </button>
-        </div>
+        <div>{renderButton()}</div> {/* 수정된 버튼 렌더링 */}
       </div>
     </>
   );
