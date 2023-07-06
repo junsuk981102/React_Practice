d1ac345f8331f95e6899424b1b784f6f56b26cb9
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-06 10:07:54 +0900

    Update screen_Register.js

--- src/screen/screen_Register.js
+++ src/screen/screen_Register.js
@@ -85,7 +85,7 @@ const ScreenRegister = (props) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100%",
+        height: "100vh",
         backgroundColor: "#E5F2F2",
         borderTop: "1px solid #00A29D",
       }}
