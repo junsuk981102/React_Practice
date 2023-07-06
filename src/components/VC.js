780a8f1fe81083fcbfdbd2ebf3fa83b2a724011e
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-06 16:15:22 +0900

    Update VC.js

--- src/components/VC.js
+++ src/components/VC.js
@@ -19,122 +19,108 @@ const VC = ({ vcObj }) => {
   }
 
   return (
-    <div>
-      <div
-        style={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "50%",
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
+    <Box>
+      {/* 상단 */}
+      <Box
+        //위치
+        position="absolute"
+        mt="0px"
+        //크기
+        w="100%"
+        h="50%"
       >
+        {/* VC 배경 사진 */}
         <Image
-          src={vcObj.vc_backgroundImg}
-          alt="vc background img"
+          //크기
           w="100%"
           h="100%"
+          //배경
+          src={vcObj.vc_backgroundImg}
+          alt="vc background img"
         />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          height: "50%",
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-        }}
+      </Box>
+      {/* 하단 */}
+      <Box
+        //위치
+        position="absolute"
+        mt="180px"
+        //크기
+        w="100%"
+        h="50%"
       >
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
-          <div
-            style={{
-              position: "absolute",
-              top: -40,
-              left: 20,
-              width: "80px",
-              height: "80px",
-              borderRadius: "20%",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          >
-            <Image
-              src={vcObj.vc_logo}
-              alt="VC logo"
-              w="100%"
-              h="100%"
-              borderRadius="20%"
-            />
-          </div>
-
-          {/* VC 이름 */}
-          <Heading
-            //위치
-            position="absolute"
-            mt="40px"
-            ml="20px"
-            //글자
-            as="h1"
-            size="lg"
-          >
-            {vcObj.vc_name}
-          </Heading>
-
-          {/* VC 정보 */}
-          <Text
-            //위치
-            position="absolute"
-            mt="80px"
-            ml="20px"
-            mr="20px"
-            //글자
-            size="md"
-          >
-            {vcObj.vc_info}
-          </Text>
-
-          {/* VC 카테고리 */}
-          <Box
-            //위치
-            position="absolute"
-            mt="5px"
-            ml="425px"
-            //배경
-            borderRadius="xl"
-            bg="primary"
-            padding="5px 10px"
-            //글자
-            as="b"
-            fontSize="xs"
-            color="white"
-          >
-            {vcObj.vc_category}
-          </Box>
-
-          {/* VC INFO 이동버튼 */}
-          <Button
-            //위치
-            position="absolute"
-            mt="30px"
-            ml="370px"
-            //배경
-            bg="none"
-            variant="none"
-            //글자
-            as="b"
-            fontSize="md"
-            onClick={() => handleClick()}
-          >
-            자세히 보기 {">"}
-          </Button>
-        </div>
-      </div>
-    </div>
+        {/* VC 로고 */}
+        <Image
+          //위치
+          position="absolute"
+          mt="-40px"
+          ml="20px"
+          //크기
+          w="80px"
+          h="80px"
+          //배경
+          borderRadius="20%"
+          src={vcObj.vc_logo}
+          alt="VC logo"
+        />
+        {/* VC 이름 */}
+        <Heading
+          //위치
+          position="absolute"
+          mt="40px"
+          ml="20px"
+          //글자
+          as="h1"
+          size="lg"
+        >
+          {vcObj.vc_name}
+        </Heading>
+        {/* VC 정보 */}
+        <Text
+          //위치
+          position="absolute"
+          mt="80px"
+          ml="20px"
+          mr="20px"
+          //글자
+          size="md"
+        >
+          {vcObj.vc_info}
+        </Text>
+        {/* VC 카테고리 */}
+        <Box
+          //위치
+          position="absolute"
+          mt="5px"
+          ml="425px"
+          //배경
+          borderRadius="xl"
+          bg="primary"
+          padding="5px 10px"
+          //글자
+          as="b"
+          fontSize="xs"
+          color="white"
+        >
+          {vcObj.vc_category}
+        </Box>
+        {/* VC INFO 이동버튼 */}
+        <Button
+          //위치
+          position="absolute"
+          mt="30px"
+          ml="370px"
+          //배경
+          bg="none"
+          variant="none"
+          //글자
+          as="b"
+          fontSize="md"
+          onClick={() => handleClick()}
+        >
+          자세히 보기 {">"}
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
