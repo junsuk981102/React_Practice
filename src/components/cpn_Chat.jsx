c26c66603d1da6e272f120da426947ce994f9b3d
kswim57 <kswim8914@gmail.com>
2023-07-07 11:25:20 +0900

    Chatting UI 변경

--- src/components/cpn_Chat.jsx
+++ src/components/cpn_Chat.jsx
@@ -5,6 +5,7 @@ import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
 import MessageSend from "./cpn_Message_Send";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 const Chat = () => {
   const [message, setMessage] = useState([]);
@@ -29,35 +30,24 @@ const Chat = () => {
   }, []);
 
   return (
-    <>
-      <section
-        style={{
-          backgroundColor: "#FFFFFF",
-          width: "70%",
-          margin: "0 auto",
-          borderRadius: "10px",
-          padding: "30px  30px 0", //상우하좌
-          height: "80vh",
-          overflow: "auto",
-        }}
-      >
-        {message &&
-          message.map((item) => <MessageFormat key={item.id} message={item} />)}
-        {user && (
-          <div
-            style={{
-              position: "sticky", // MessageSend 컴포넌트의 위치를 sticky로 설정
-              bottom: 0, // 하단 여백 조절
-              left: 0, // 왼쪽 정렬
-              width: "100%", // 너비를 부모에 맞게 설정
-            }}
-          >
-            <MessageSend scroll={scroll} />
-          </div>
-        )}
-        <span ref={scroll}></span>
-      </section>
-    </>
+    <Box
+      bg="#FFFFFF"
+      width="70%"
+      margin="0 auto"
+      borderRadius="10px"
+      padding="30px 30px 0"
+      height="80vh"
+      overflow="auto"
+    >
+      {message &&
+        message.map((item) => <MessageFormat key={item.id} message={item} />)}
+      {user && (
+        <Box position="sticky" bottom={0} left={0} width="100%">
+          <MessageSend scroll={scroll} />
+        </Box>
+      )}
+      <span ref={scroll}></span>
+    </Box>
   );
 };
 
--- src/components/cpn_Message_Format.jsx
+++ src/components/cpn_Message_Format.jsx
@@ -1,6 +1,12 @@
-import { auth } from "../firebase-config";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const MessageFormat = ({ message }) => {
+  const auth = getAuth();
+  const [user] = useAuthState(auth);
+  const defaultProfileImage = "/image/user.png"; // 기본 디폴트 이미지 경로
+  const photo = user?.photoURL || defaultProfileImage;
+
   let newStyles = "";
   if (auth.currentUser) {
     if (message.uid === auth.currentUser.uid) {
@@ -35,7 +41,7 @@ const MessageFormat = ({ message }) => {
         </div>
         <p className="user">{`${newDate} . ${time}`}</p>
       </div>
-      <img src={message.photo} alt="user profile" />
+      <img src={message.photo || defaultProfileImage} alt="user profile" />
     </article>
   );
 };
