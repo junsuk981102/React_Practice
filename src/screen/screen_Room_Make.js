ebef1c92c0815a24621253c0ae07e798a457dad0
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-10 16:52:22 +0900

    Update screen_Room_Make.js

--- src/screen/screen_Room_Make.js
+++ src/screen/screen_Room_Make.js
@@ -1,16 +1,7 @@
-// import React from 'react';
-// import {useNavigate} from "react-router-dom";
-
 import React, { useState } from "react";
 import { dbService } from "../firebase-config";
 
 const ScreenRoomMake = (props) => {
-  // const navi = useNavigate();
-
-  // function handleClick(text) {
-  //     navi(`${text}`)
-  // }
-
   const [name, setName] = useState("");
   const [category, setCategory] = useState("");
   const [information, setInformation] = useState("");
@@ -126,10 +117,6 @@ const ScreenRoomMake = (props) => {
           <input type="submit" value="커뮤니티 만들기" />
         </form>
       </div>
-
-      {/* <button onClick={() => handleClick("/screen_room_list")}>뒤로가기</button>
-            <br/><br/>
-            <button onClick={() => handleClick("/screen_room_setting")}>screen_room_setting 페이지</button> */}
     </>
   );
 };
