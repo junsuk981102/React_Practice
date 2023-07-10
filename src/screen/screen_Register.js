45eb8f4dc5cf689839d03fc89ea4053115bf7b04
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-10 15:28:27 +0900

    Update screen_Register.js

--- src/screen/screen_Register.js
+++ src/screen/screen_Register.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import Register from "../components/cpn_Register";
+import { Box } from "@chakra-ui/react";
 
-const ScreenRegister = (props) => {
-  const navi = useNavigate();
-
+const ScreenRegister = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#E5F2F2",
-        borderTop: "1px solid #00A29D",
-      }}
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      h="100vh"
+      bg="#E5F2F2"
+      borderTop="1px solid #00A29D"
     >
       <Register />
-    </div>
+    </Box>
   );
 };
 
