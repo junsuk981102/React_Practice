100587d401c3e902f8d8fc8f276ca69f4f0d9713
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-10 14:36:21 +0900

    Update screen_Chat.js

--- src/screen/screen_Chat.js
+++ src/screen/screen_Chat.js
@@ -1,29 +1,20 @@
 import Chat from "../components/cpn_Chat";
-import "../chat.css";
-import { useNavigate, useLocation } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 function ScreenChat() {
-  const { state } = useLocation();
-  console.log(state.name);
-
-  const navi = useNavigate();
-
-  function handleClick(text) {
-    navi(`${text}`);
-  }
-
   return (
     <>
-      <div style={{ backgroundColor: "#E5F2F2", minHeight: "100vh" }}>
-        {/* <h3>ScreenChat 페이지입니다.</h3>
-        <h4>{state.name}의 커뮤니티 입니다.</h4> 
-        <button onClick={() => handleClick("/screen_room_list")}>
-          뒤로가기
-  </button>*/}
-        <div className="ScreenChat" style={{ padding: "50px" }}>
+      <Box
+        //크기
+        h="100vh"
+        //배경
+        bg="#E5F2F2"
+        borderTop="1px solid #00A29D"
+      >
+        <Box p="50px">
           <Chat />
-        </div>
-      </div>
+        </Box>
+      </Box>
     </>
   );
 }
