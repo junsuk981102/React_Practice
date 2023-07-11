9bb2e6e842be33338973050611987dfb2d8734c7
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-11 22:25:10 +0900

    Update screen_Header.js

--- src/screen/screen_Header.js
+++ src/screen/screen_Header.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
-
 import { Box, Image, Button } from "@chakra-ui/react";
 
 function Header(props) {
