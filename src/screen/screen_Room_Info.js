1ae59d10dd8876246bd4c10681acc01caf14b6df
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-12 23:36:10 +0900

    update

--- src/screen/screen_Room_Info.js
+++ src/screen/screen_Room_Info.js
@@ -45,228 +45,225 @@ const ScreenRoomInfo = (props) => {
 
   return (
     <>
-      <Box bg="#E5F2F2" h="100vh" borderTop="1px solid #00A29D">
-        <Box pl={boxPaddingLeft} pr={boxPaddingRight}>
-          <Heading
-            //위치
-            mt="30px"
-            mb="30px"
-            //크기
-            as="h1"
-            size="lg"
-          >
-            커뮤니티 소개
-          </Heading>
+      <Box
+        pl={boxPaddingLeft}
+        pr={boxPaddingRight}
+        bg="#E5F2F2"
+        h="100vh"
+        borderTop="1px solid #00A29D"
+      >
+        <Heading
+          //위치
+          mt="30px"
+          mb="30px"
+          //크기
+          as="h1"
+          size="lg"
+        >
+          커뮤니티 소개
+        </Heading>
 
-          <Box
-            w="1000px"
-            h="auto"
-            p="30px"
-            bg="white"
-            border="3px solid #00A29D"
-            borderRadius="lg"
-          >
-            <Box
-              h="auto"
-              display="flex"
-              position="relative"
-              alignItems="center"
-            >
-              <Image
-                src={state.com_profileImg}
-                w="200px"
-                h="200px"
-                objectFit="cover"
-                borderRadius="50%"
-              />
-              <Box ml="25px">
-                <Heading
-                  //글자
-                  as="h1"
-                  size="lg"
-                >
-                  {state.com_name}
-                </Heading>
-                <Box
-                  w="100px"
-                  h="auto"
-                  p="5px"
-                  mt="10px"
-                  mb="10px"
-                  bg="#00A29D"
-                  borderRadius="xl"
-                  textAlign="center"
-                >
-                  <Text fontSize="md" as="b" color="white">
-                    {state.com_category}
-                  </Text>
-                </Box>
-                <Box>
-                  <Text fontSize="md" as="b">
-                    {state.com_info}
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-
-            <Box h="auto" ml="20px" mt="30px">
-              <Text fontSize="lg" as="b" mr="20px">
-                투자 목표 금액
-              </Text>
-              <Text fontSize="lg" as="b" color="#00A29D">
-                {(state.com_now_investment / state.com_total_investment) * 100}%
-                달성
-              </Text>
-              <Box position="relative" mt="20px">
-                <Box
-                  position="absolute"
-                  top="0"
-                  left="0"
-                  w="90%"
-                  h="45px"
-                  bg="white"
-                  borderRadius="22.5px"
-                  border="1px solid #00A29D"
-                ></Box>
-                <Box
-                  position="absolute"
-                  top="0"
-                  left="0"
-                  w={
-                    (state.com_now_investment / state.com_total_investment) *
-                      90 +
-                    "%"
-                  }
-                  h="45px"
-                  bg={"linear-gradient(to right, #00A29D, #FFFFFF)"}
-                  bo
-                  borderRadius="22.5px"
-                  border="1px solid #00A29D"
-                ></Box>
-              </Box>
-
-              <Box position="relative" color="#00A29D">
-                <Box position="absolute" top="55" left="85%">
-                  <Text as="b">
-                    <NumberFormat number={state.com_total_investment} />원
-                  </Text>
-                </Box>
-                <Box
-                  position="absolute"
-                  top="55"
-                  left={
-                    (state.com_now_investment / state.com_total_investment) *
-                      80 +
-                    "%"
-                  }
-                >
-                  <Text as="b">
-                    <NumberFormat number={state.com_now_investment} />원
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-
-            <Box display="flex" justifyItems="center" mt="120px" ml="20px">
-              <Box mr="50px">
-                <Text fontSize="lg" as="b">
-                  티켓 구매하기
-                </Text>
-
-                <Box display="flex" mt="20px">
-                  <Box>
-                    <Image
-                      src="../image/ticketgrey.png"
-                      w="150px"
-                      h="150px"
-                      mb="15px"
-                    />
-                    <Box
-                      w="150px"
-                      h="25px"
-                      p="5px"
-                      fontSize="xs"
-                      color="#00A29D"
-                      textAlign="center"
-                      bg="#E5F2F2"
-                      border="1px solid black"
-                      borderRadius="22.5px"
-                    >
-                      <Text>현재 보유한 티켓 수 : 5매</Text>
-                    </Box>
-                  </Box>
-                  <Box ml="30px" textAlign="center" fontWeight="bold">
-                    <Text mb="15px" fontSize="xl">
-                      <NumberFormat number={state.com_ticket_price} />원
-                    </Text>
-                    <Button
-                      w="120px"
-                      h="35px"
-                      p="10px"
-                      mt="15px"
-                      color="white"
-                      fontSize="sm"
-                      bg="#00A29D"
-                      borderRadius="17.5px"
-                      onClick={() => handleClick()}
-                    >
-                      구매하기
-                    </Button>
-                    <Text mt="5px" color="grey" fontSize="2xs">
-                      * 최대 {state.com_ticket_max}매까지 구매 가능
-                    </Text>
-                  </Box>
-                </Box>
-              </Box>
-
-              <Box>
-                <Text fontSize="lg" as="b" mb="15px">
-                  관심있는 회사
-                </Text>
-                <Box display="flex">
-                  <Image w="154px" h="185px" src="../image/startup1.png" />
-                  <Image w="154px" h="185px" src="../image/startup2.png" />
-                  <Image w="154px" h="185px" src="../image/startup3.png" />
-                </Box>
-              </Box>
-            </Box>
-
-            <Box
-              display="flex"
-              justifyContent="center"
-              textAlign="center"
-              mt="100px"
-            >
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                w="120px"
-                h="30px"
-                bg="#E5F2F2"
-                fontSize="md"
-                fontWeight="bold"
-                border="1px solid black"
-                borderRadius="xl"
+        <Box
+          w="1000px"
+          h="auto"
+          p="30px"
+          bg="white"
+          border="3px solid #00A29D"
+          borderRadius="lg"
+        >
+          <Box h="auto" display="flex" position="relative" alignItems="center">
+            <Image
+              src={state.com_profileImg}
+              w="200px"
+              h="200px"
+              objectFit="cover"
+              borderRadius="50%"
+            />
+            <Box ml="25px">
+              <Heading
+                //글자
+                as="h1"
+                size="lg"
               >
-                <Text>{state.com_member}명 참여중</Text>
-              </Box>
-            </Box>
-
-            <Box display="flex" justifyContent="center" p="20px">
-              <Button
-                w="600px"
-                h="60px"
-                fontSize="lg"
-                fontWeight="bold"
-                color="white"
+                {state.com_name}
+              </Heading>
+              <Box
+                w="100px"
+                h="auto"
+                p="5px"
+                mt="10px"
+                mb="10px"
                 bg="#00A29D"
                 borderRadius="xl"
-                onClick={() => handleClick_chat()}
+                textAlign="center"
               >
-                커뮤니티 참여하기
-              </Button>
+                <Text fontSize="md" as="b" color="white">
+                  {state.com_category}
+                </Text>
+              </Box>
+              <Box>
+                <Text fontSize="md" as="b">
+                  {state.com_info}
+                </Text>
+              </Box>
             </Box>
+          </Box>
+
+          <Box h="auto" ml="20px" mt="30px">
+            <Text fontSize="lg" as="b" mr="20px">
+              투자 목표 금액
+            </Text>
+            <Text fontSize="lg" as="b" color="#00A29D">
+              {(state.com_now_investment / state.com_total_investment) * 100}%
+              달성
+            </Text>
+            <Box position="relative" mt="20px">
+              <Box
+                position="absolute"
+                top="0"
+                left="0"
+                w="90%"
+                h="45px"
+                bg="white"
+                borderRadius="22.5px"
+                border="1px solid #00A29D"
+              ></Box>
+              <Box
+                position="absolute"
+                top="0"
+                left="0"
+                w={
+                  (state.com_now_investment / state.com_total_investment) * 90 +
+                  "%"
+                }
+                h="45px"
+                bg={"linear-gradient(to right, #00A29D, #FFFFFF)"}
+                bo
+                borderRadius="22.5px"
+                border="1px solid #00A29D"
+              ></Box>
+            </Box>
+
+            <Box position="relative" color="#00A29D">
+              <Box position="absolute" top="55" left="85%">
+                <Text as="b">
+                  <NumberFormat number={state.com_total_investment} />원
+                </Text>
+              </Box>
+              <Box
+                position="absolute"
+                top="55"
+                left={
+                  (state.com_now_investment / state.com_total_investment) * 80 +
+                  "%"
+                }
+              >
+                <Text as="b">
+                  <NumberFormat number={state.com_now_investment} />원
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+
+          <Box display="flex" justifyItems="center" mt="120px" ml="20px">
+            <Box mr="50px">
+              <Text fontSize="lg" as="b">
+                티켓 구매하기
+              </Text>
+
+              <Box display="flex" mt="20px">
+                <Box>
+                  <Image
+                    src="../image/ticketgrey.png"
+                    w="150px"
+                    h="150px"
+                    mb="15px"
+                  />
+                  <Box
+                    w="150px"
+                    h="25px"
+                    p="5px"
+                    fontSize="xs"
+                    color="#00A29D"
+                    textAlign="center"
+                    bg="#E5F2F2"
+                    border="1px solid black"
+                    borderRadius="22.5px"
+                  >
+                    <Text>현재 보유한 티켓 수 : 5매</Text>
+                  </Box>
+                </Box>
+                <Box ml="30px" textAlign="center" fontWeight="bold">
+                  <Text mb="15px" fontSize="xl">
+                    <NumberFormat number={state.com_ticket_price} />원
+                  </Text>
+                  <Button
+                    w="120px"
+                    h="35px"
+                    p="10px"
+                    mt="15px"
+                    color="white"
+                    fontSize="sm"
+                    bg="#00A29D"
+                    borderRadius="17.5px"
+                    onClick={() => handleClick()}
+                  >
+                    구매하기
+                  </Button>
+                  <Text mt="5px" color="grey" fontSize="2xs">
+                    * 최대 {state.com_ticket_max}매까지 구매 가능
+                  </Text>
+                </Box>
+              </Box>
+            </Box>
+
+            <Box>
+              <Text fontSize="lg" as="b" mb="15px">
+                관심있는 회사
+              </Text>
+              <Box display="flex">
+                <Image w="154px" h="185px" src="../image/startup1.png" />
+                <Image w="154px" h="185px" src="../image/startup2.png" />
+                <Image w="154px" h="185px" src="../image/startup3.png" />
+              </Box>
+            </Box>
+          </Box>
+
+          <Box
+            display="flex"
+            justifyContent="center"
+            textAlign="center"
+            mt="100px"
+          >
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              w="120px"
+              h="30px"
+              bg="#E5F2F2"
+              fontSize="md"
+              fontWeight="bold"
+              border="1px solid black"
+              borderRadius="xl"
+            >
+              <Text>{state.com_member}명 참여중</Text>
+            </Box>
+          </Box>
+
+          <Box display="flex" justifyContent="center" p="20px">
+            <Button
+              w="600px"
+              h="60px"
+              fontSize="lg"
+              fontWeight="bold"
+              color="white"
+              bg="#00A29D"
+              borderRadius="xl"
+              onClick={() => handleClick_chat()}
+            >
+              커뮤니티 참여하기
+            </Button>
           </Box>
         </Box>
       </Box>
--- src/screen/screen_Startup_Info.js
+++ src/screen/screen_Startup_Info.js
@@ -6,6 +6,7 @@ import {
   Heading,
   Image,
   useBreakpointValue,
+  Divider,
 } from "@chakra-ui/react";
 
 const ScreenStartupInfo = () => {
@@ -18,8 +19,8 @@ const ScreenStartupInfo = () => {
       <Box
         pl={boxPaddingLeft}
         pr={boxPaddingRight}
-        h="100vh"
         bg="#E5F2F2"
+        h="100vh"
         borderTop="1px solid #00A29D"
       >
         <Heading
@@ -34,49 +35,154 @@ const ScreenStartupInfo = () => {
         </Heading>
 
         <Box
-          w="1000px"
-          h="900px"
+          w="800px"
+          h="auto"
           p="30px"
           bg="white"
           border="3px solid #00A29D"
           borderRadius="lg"
         >
-          <Box display="flex" position="relative" alignItems="center">
+          <Box h="auto" display="flex" position="relative" alignItems="center">
             <Image
               src={state.sup_logo}
-              alt="logo"
               w="200px"
               h="200px"
+              objectFit="cover"
               borderRadius="lg"
             />
-            <Text
-              //위치
-              ml="20px"
-              //크기
-              as="b"
-              fontSize="5xl"
-            >
-              {state.sup_name}
-            </Text>
+            <Box ml="25px">
+              <Heading
+                //글자
+                as="h1"
+                size="lg"
+              >
+                {state.sup_name}
+              </Heading>
+              <Box
+                w="150px"
+                h="auto"
+                p="5px"
+                mt="10px"
+                mb="10px"
+                bg="#00A29D"
+                borderRadius="xl"
+                textAlign="center"
+              >
+                <Text fontSize="md" as="b" color="white">
+                  {state.sup_category}
+                </Text>
+              </Box>
+              <Box>
+                <Text fontSize="md" as="b">
+                  {state.sup_info}
+                </Text>
+              </Box>
+            </Box>
           </Box>
 
-          <Box
-            mt="15px"
-            w="200px"
-            bg="#00A29D"
-            borderRadius="lg"
-            p="5px"
-            textAlign="center"
-          >
-            <Text as="b" color="white" fontSize="lg">
-              {state.sup_category}
+          <Box ml="20px" mt="30px">
+            <Text fontSize="lg" as="b">
+              기업정보
             </Text>
+            <Box display="flex" flexDirection="row" ml="10px" mt="25px">
+              <Box display="flex" flexDirection="row" mr="150px">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">대표자</Text>
+                  <Text mb="10px">국적</Text>
+                  <Text>회사구분</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">최현일</Text>
+                  <Text mb="10px">한국</Text>
+                  <Text>중소기업</Text>
+                </Box>
+              </Box>
+
+              <Box display="flex" flexDirection="row">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">발행주식</Text>
+                  <Text mb="10px">업종</Text>
+                  <Text>홈페이지</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">통일 주권</Text>
+                  <Text mb="10px">데이터베이스 및 온라인 정보 제공업</Text>
+                  <Text>http://www.peopet.co.kr</Text>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          <Divider mt="20px" />
+
+          <Box h="auto" ml="20px" mt="20px" display="flex" flexDirection="row">
+            <Box mr="190px">
+              <Text fontSize="lg" as="b">
+                투자정보
+              </Text>
+              <Box display="flex" flexDirection="row" ml="10px" mt="25px">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">최종투자 단계</Text>
+                  <Text mb="10px">누적투자 금액</Text>
+                  <Text>투자유치 횟수</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">Seed</Text>
+                  <Text mb="10px">1억 미만</Text>
+                  <Text>3회</Text>
+                </Box>
+              </Box>
+            </Box>
+
+            <Box>
+              <Text fontSize="lg" as="b">
+                투자자
+              </Text>
+              <Box display="flex" flexDirection="row" ml="10px" mt="20px">
+                <Image w="90px" h="110px" src="../image/startup1.png" />
+                <Image w="90px" h="110px" src="../image/startup2.png" />
+                <Image w="90px" h="110px" src="../image/startup3.png" />
+              </Box>
+            </Box>
           </Box>
 
-          <Box mt="30px">
-            <Text as="b" fontSize="2xl">
-              {state.sup_info}
+          <Divider mt="20px" />
+
+          <Box h="auto" ml="20px" mt="20px">
+            <Text fontSize="lg" as="b">
+              서비스/제품
             </Text>
+            <Box display="flex" flexDirection="row" ml="10px" mt="25px">
+              <Image
+                src={state.sup_logo}
+                w="60px"
+                h="60px"
+                objectFit="cover"
+                borderRadius="30px"
+              />
+              <Box display="flex" flexDirection="column" mt="5px" ml="10px">
+                <Text fontSize="lg" as="b">
+                  페오펫
+                </Text>
+                <Text color="#00A29D" fontSize="sm" as="b">
+                  간편 모바일 반려동물등록 서비스
+                </Text>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
