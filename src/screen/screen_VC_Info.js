005161a1d616018db7b57a01ad11d1f083ffc2b1
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-12 23:41:16 +0900

    Update screen_VC_Info.js

--- src/screen/screen_VC_Info.js
+++ src/screen/screen_VC_Info.js
@@ -6,6 +6,7 @@ import {
   Heading,
   Image,
   useBreakpointValue,
+  Divider,
 } from "@chakra-ui/react";
 
 const ScreenVCInfo = () => {
@@ -18,8 +19,8 @@ const ScreenVCInfo = () => {
       <Box
         pl={boxPaddingLeft}
         pr={boxPaddingRight}
-        h="100vh"
         bg="#E5F2F2"
+        h="100vh"
         borderTop="1px solid #00A29D"
       >
         <Heading
@@ -34,49 +35,129 @@ const ScreenVCInfo = () => {
         </Heading>
 
         <Box
-          w="1000px"
-          h="900px"
+          w="800px"
+          h="auto"
           p="30px"
           bg="white"
           border="3px solid #00A29D"
           borderRadius="lg"
         >
-          <Box display="flex" position="relative" alignItems="center">
+          <Box h="auto" display="flex" position="relative" alignItems="center">
             <Image
               src={state.vc_logo}
-              alt="logo"
               w="200px"
               h="200px"
+              objectFit="cover"
               borderRadius="lg"
             />
-            <Text
-              //위치
-              ml="20px"
-              //크기
-              as="b"
-              fontSize="5xl"
-            >
-              {state.vc_name}
-            </Text>
+            <Box ml="25px">
+              <Heading
+                //글자
+                as="h1"
+                size="lg"
+              >
+                {state.vc_name}
+              </Heading>
+              <Box
+                w="150px"
+                h="auto"
+                p="5px"
+                mt="10px"
+                mb="10px"
+                bg="#00A29D"
+                borderRadius="xl"
+                textAlign="center"
+              >
+                <Text fontSize="md" as="b" color="white">
+                  {state.vc_category}
+                </Text>
+              </Box>
+              <Box>
+                <Text fontSize="md" as="b">
+                  {state.vc_info}
+                </Text>
+              </Box>
+            </Box>
           </Box>
 
-          <Box
-            mt="15px"
-            w="200px"
-            bg="#00A29D"
-            borderRadius="lg"
-            p="5px"
-            textAlign="center"
-          >
-            <Text as="b" color="white" fontSize="lg">
-              {state.vc_category}
+          <Box ml="20px" mt="30px">
+            <Text fontSize="lg" as="b">
+              기업정보
             </Text>
-          </Box>
+            <Box display="flex" flexDirection="row" ml="10px" mt="25px">
+              <Box display="flex" flexDirection="row" mr="150px">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">대표자</Text>
+                  <Text mb="10px">국적</Text>
+                  <Text>회사구분</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">한킴</Text>
+                  <Text mb="10px">미국</Text>
+                  <Text>벤처캐피탈</Text>
+                </Box>
+              </Box>
 
-          <Box mt="30px">
-            <Text as="b" fontSize="2xl">
-              {state.vc_info}
-            </Text>
+              <Box display="flex" flexDirection="row">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">법인분류</Text>
+                  <Text mb="10px">업력</Text>
+                  <Text>홈페이지</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">주식회사</Text>
+                  <Text mb="10px">27.5년차</Text>
+                  <Text>http://altos.vc/</Text>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          <Divider mt="20px" />
+
+          <Box h="auto" ml="20px" mt="20px" display="flex" flexDirection="row">
+            <Box mr="190px">
+              <Text fontSize="lg" as="b">
+                투자정보(2015년 이후)
+              </Text>
+              <Box display="flex" flexDirection="row" ml="10px" mt="25px">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  mr="50px"
+                  color="grey"
+                >
+                  <Text mb="10px">총 선탠 건수</Text>
+                  <Text mb="10px">총 집핸 금액</Text>
+                  <Text>운용 펀드</Text>
+                </Box>
+                <Box display="flex" flexDirection="column">
+                  <Text mb="10px">146건</Text>
+                  <Text mb="10px">9125억+</Text>
+                  <Text>4개</Text>
+                </Box>
+              </Box>
+            </Box>
+
+            <Box>
+              <Text fontSize="lg" as="b">
+                주요 투자 대상
+              </Text>
+              <Box display="flex" flexDirection="row" ml="10px" mt="20px">
+                <Image w="90px" h="110px" src="../image/startup1.png" />
+                <Image w="90px" h="110px" src="../image/startup2.png" />
+                <Image w="90px" h="110px" src="../image/startup3.png" />
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
