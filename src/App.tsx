7d09ae58afbac464f5f81055424f6cfd64e4cf2f
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-13 13:57:42 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider, Flex } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import theme from "./theme";
+import { Outlet } from "react-router-dom";
 
 import ScreenChat from "./screen/screen_Chat";
 import ScreenExample from "./screen/screen_Example";
@@ -25,6 +26,16 @@ import ScreenTest from "./screen/screen_Test";
 import ScreenNewsletter from "./screen/screen_Newsletter";
 
 export default function App() {
+  const MainLayout = () => {
+    return (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
   const [account, setAccount] = useState<string>("");
 
   const getAccount = async () => {
@@ -49,51 +60,54 @@ export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <div className={"app"} style={{ flex: 1 }}>
-        <Header />
         <Routes>
-          <Route path="/" element={<ScreenMain />}></Route>
-          <Route path="/screen_chat/*" element={<ScreenChat />}></Route>
-          <Route path="/screen_Example/*" element={<ScreenExample />}></Route>
-          <Route path="/screen_login/*" element={<ScreenLogin />}></Route>
-          <Route
-            path="/screen_minting/*"
-            element={<ScreenMinting account={account} />}
-          ></Route>
-          <Route
-            path="/screen_my_governance/*"
-            element={<ScreenMyGovernance account={account} />}
-          ></Route>
-          <Route path="/screen_profile/*" element={<ScreenProfile />}></Route>
-          <Route path="/screen_register/*" element={<ScreenRegister />}></Route>
-          <Route
-            path="/screen_room_info/*"
-            element={<ScreenRoomInfo />}
-          ></Route>
-          <Route
-            path="/screen_room_list/*"
-            element={<ScreenRoomList />}
-          ></Route>
-          <Route
-            path="/screen_room_make/*"
-            element={<ScreenRoomMake />}
-          ></Route>
-          <Route
-            path="/screen_startup_info/*"
-            element={<ScreenStartupInfo />}
-          ></Route>
-          <Route path="/screen_vc_info/*" element={<ScreenVCInfo />}></Route>
-          <Route
-            path="/screen_wallet_connect/*"
-            element={<ScreenWalletConnect />}
-          ></Route>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<ScreenMain />}></Route>
+            <Route path="/screen_chat/*" element={<ScreenChat />}></Route>
+            <Route path="/screen_Example/*" element={<ScreenExample />}></Route>
+            <Route path="/screen_login/*" element={<ScreenLogin />}></Route>
+            <Route
+              path="/screen_minting/*"
+              element={<ScreenMinting account={account} />}
+            ></Route>
+            <Route
+              path="/screen_my_governance/*"
+              element={<ScreenMyGovernance account={account} />}
+            ></Route>
+            <Route path="/screen_profile/*" element={<ScreenProfile />}></Route>
+            <Route
+              path="/screen_register/*"
+              element={<ScreenRegister />}
+            ></Route>
+            <Route
+              path="/screen_room_info/*"
+              element={<ScreenRoomInfo />}
+            ></Route>
+            <Route
+              path="/screen_room_list/*"
+              element={<ScreenRoomList />}
+            ></Route>
+            <Route
+              path="/screen_room_make/*"
+              element={<ScreenRoomMake />}
+            ></Route>
+            <Route
+              path="/screen_startup_info/*"
+              element={<ScreenStartupInfo />}
+            ></Route>
+            <Route path="/screen_vc_info/*" element={<ScreenVCInfo />}></Route>
+            <Route
+              path="/screen_wallet_connect/*"
+              element={<ScreenWalletConnect />}
+            ></Route>
+            <Route path="/screen_test/*" element={<ScreenTest />}></Route>
+          </Route>
 
-          <Route path="/screen_test/*" element={<ScreenTest />}></Route>
           <Route
             path="/screen_newsletter/*"
             element={<ScreenNewsletter />}
           ></Route>
         </Routes>
-        <Footer />
       </div>
     </ChakraProvider>
   );
