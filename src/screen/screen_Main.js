314d93ae1648849b0905ed69c53e49ae12ec2bd6
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-17 18:12:48 +0900

    Update screen_Main.js

--- src/screen/screen_Main.js
+++ src/screen/screen_Main.js
@@ -10,6 +10,10 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const ScreenMain = (props) => {
   const boxPaddingLeft = useBreakpointValue({ base: "20px", xl: "200px" });
   const boxPaddingRight = useBreakpointValue({ base: "20x", xl: "200px" });
@@ -38,16 +42,17 @@ const ScreenMain = (props) => {
 
   const renderStartups = () => {
     return startups.map((startup) => (
-      <Box
-        key={startup.id}
-        w="500px"
-        h="360px"
-        borderRadius="10px"
-        position="relative"
-        overflow="hidden"
-        boxShadow="0 0 15px #00A29D"
-      >
-        <Startup startupObj={startup} />
+      <Box key={startup.id} style={{ margin: "20px" }}>
+        <Box
+          w="500px"
+          h="360px"
+          borderRadius="10px"
+          position="relative"
+          overflow="hidden"
+          boxShadow="0 0 15px #00A29D"
+        >
+          <Startup startupObj={startup} />
+        </Box>
       </Box>
     ));
   };
@@ -68,6 +73,23 @@ const ScreenMain = (props) => {
     ));
   };
 
+  const SlideList = () => {
+    // react-slick 설정 옵션
+    const settings = {
+      dots: true, // 페이지 표시 여부
+      infinite: true, // 무한 루프 여부
+      speed: 500, // 슬라이드 전환 속도
+      slidesToShow: 2, // 보여질 슬라이드 개수
+      slidesToScroll: 1, // 슬라이드 이동시 이동하는 슬라이드 개수
+    };
+
+    return (
+      <Box>
+        <Slider {...settings}>{renderStartups()}</Slider>{" "}
+      </Box>
+    );
+  };
+
   return (
     <>
       <Image src="image/stot_main.png" alt="main logo" w="100%" h="auto" />
@@ -85,6 +107,7 @@ const ScreenMain = (props) => {
         >
           {renderStartups()}
         </Grid>
+        <SlideList />
         <Heading as="h2" size="md" mt="50px" mb="5px">
           새로운 VC 찾기
         </Heading>
