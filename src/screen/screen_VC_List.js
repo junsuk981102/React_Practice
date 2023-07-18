ecba95aa4e412b68b8f1ef44728b0e5c929a3493
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-18 11:28:09 +0900

    VC 페이지 완성

--- src/screen/screen_VC_List.js
+++ src/screen/screen_VC_List.js
@@ -1,40 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "../firebase-config";
-import Community from "../components/Community";
+import VC from "../components/VC";
 import { Heading, Grid, Box, useBreakpointValue } from "@chakra-ui/react";
 
 const ScreenVCList = (props) => {
-  const [communities, setCommunities] = useState([]);
+  const [vcs, setVCs] = useState([]);
   const boxPaddingLeft = useBreakpointValue({ base: "20px", xl: "200px" });
   const boxPaddingRight = useBreakpointValue({ base: "20x", xl: "200px" });
 
   useEffect(() => {
-    dbService.collection("community_list").onSnapshot((snapshot) => {
-      const communityArray = snapshot.docs.map((doc) => ({
+    dbService.collection("vc_list").onSnapshot((snapshot) => {
+      const vcArray = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      setCommunities(communityArray);
+      setVCs(vcArray);
     });
   }, []);
 
-  const renderCommunities = () => {
-    return communities.map((community) => (
-      <Box
-        //커뮤니티 ID
-        key={community.id}
-        //크기
-        w="300px"
-        h="400px"
-        //배경
-        bg="white"
-        borderRadius="md"
-        boxShadow="0 0 15px #00A29D"
-        //구조
-        position="relative"
-        overflow="hidden"
-      >
-        <Community communityObj={community} />
+  const renderVCs = () => {
+    return vcs.map((vc) => (
+      <Box key={vc.id}>
+        <Box
+          w="500px"
+          h="360px"
+          borderRadius="10px"
+          position="relative"
+          overflow="hidden"
+          boxShadow="0 0 15px #00A29D"
+          m="20px"
+        >
+          <VC vcObj={vc} />
+        </Box>
       </Box>
     ));
   };
@@ -48,26 +45,18 @@ const ScreenVCList = (props) => {
       pb="100px"
       borderTop="1px solid #00A29D"
     >
-      <Box pl={boxPaddingLeft} pr={boxPaddingRight}>
-        {/* 페이지 제목 */}
-        <Heading
-          //위치
-          mt="30px"
-          mb="30px"
-          //크기
-          as="h1"
-          size="lg"
-        >
-          커뮤니티
+      <Box pl={boxPaddingLeft} pr={boxPaddingRight} pb="50px">
+        <Heading as="h2" size="md" mt="30px" mb="5px">
+          새로운 VC 찾기
         </Heading>
-
-        {/* 커뮤니티 리스트 */}
+        <Heading as="h1" size="lg" mb="20px">
+          투자 중인 VC
+        </Heading>
         <Grid
-          //형식
-          gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))"
-          gap="5px"
+          gridTemplateColumns="repeat(auto-fit, minmax(500px, 1fr))"
+          gap="20px"
         >
-          {renderCommunities()}
+          {renderVCs()}
         </Grid>
       </Box>
     </Box>
