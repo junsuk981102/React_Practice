8af01e989bc6bcc781ef522e1c1fc968e433ea3d
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-18 13:22:27 +0900

    커뮤니티 리스트 슬라이드 추가

--- src/screen/screen_Main.js
+++ src/screen/screen_Main.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "../firebase-config";
+import Community from "../components/Community";
 import Startup from "../components/Startup";
 import VC from "../components/VC";
 import {
@@ -17,8 +18,20 @@ import "slick-carousel/slick/slick-theme.css";
 const ScreenMain = (props) => {
   const boxPaddingLeft = useBreakpointValue({ base: "20px", xl: "200px" });
   const boxPaddingRight = useBreakpointValue({ base: "20px", xl: "200px" });
+  const [communities, setCommunities] = useState([]);
+
   const [startups, setStartups] = useState([]);
   const [vcs, setVCs] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("community_list").onSnapshot((snapshot) => {
+      const communityArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCommunities(communityArray);
+    });
+  }, []);
 
   useEffect(() => {
     dbService.collection("startup_list").onSnapshot((snapshot) => {
@@ -39,6 +52,28 @@ const ScreenMain = (props) => {
       setVCs(vcArray);
     });
   }, []);
+
+  const renderCommunities = () => {
+    return communities.map((community) => (
+      <Box key={community.id}>
+        <Box
+          //크기
+          w="300px"
+          h="400px"
+          //배경
+          bg="white"
+          borderRadius="md"
+          boxShadow="0 0 15px #00A29D"
+          m="20px"
+          //구조
+          position="relative"
+          overflow="hidden"
+        >
+          <Community communityObj={community} />
+        </Box>
+      </Box>
+    ));
+  };
 
   const renderStartups = () => {
     return startups.map((startup) => (
@@ -74,6 +109,25 @@ const ScreenMain = (props) => {
         </Box>
       </Box>
     ));
+  };
+
+  const SlideList_com = () => {
+    // react-slick 설정 옵션
+    const settings = {
+      dots: true, // 페이지 표시 여부
+      infinite: true, // 무한 루프 여부
+      speed: 500, // 슬라이드 전환 속도
+      slidesToShow: 3, // 보여질 슬라이드 개수
+      slidesToScroll: 1, // 슬라이드 이동시 이동하는 슬라이드 개수
+      autoplay: true, // Enable automatic sliding
+      autoplaySpeed: 3000, // Set the interval (in milliseconds) between each slide transition
+    };
+
+    return (
+      <Box>
+        <Slider {...settings}>{renderCommunities()}</Slider>
+      </Box>
+    );
   };
 
   const SlideList_sup = () => {
@@ -118,7 +172,14 @@ const ScreenMain = (props) => {
     <>
       <Image src="image/stot_main.png" alt="main logo" w="100%" h="auto" />
 
-      <Box pl={boxPaddingLeft} pr={boxPaddingRight} pb="50px">
+      <Box pl={boxPaddingLeft} pr={boxPaddingRight} pb="100px">
+        <Heading as="h2" size="md" mt="50px" mb="5px">
+          새로운 커뮤니티 찾기
+        </Heading>
+        <Heading as="h1" size="lg" mb="20px">
+          투자 중인 커뮤니티
+        </Heading>
+        <SlideList_com />
         <Heading as="h2" size="md" mt="50px" mb="5px">
           새로운 스타트업 찾기
         </Heading>
