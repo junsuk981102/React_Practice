2837d35ddff003a1957c6f5b49a23ecc56780124
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-21 14:46:31 +0900

    VC 1차 코드 수정

--- src/components/VC.js
+++ src/components/VC.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Image, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Image, Button, Heading, Text, Flex } from "@chakra-ui/react";
 
 const VC = ({ vcObj }) => {
   const navi = useNavigate();
@@ -19,15 +19,22 @@ const VC = ({ vcObj }) => {
   }
 
   return (
-    <Box>
+    <Flex
+      //정렬
+      position="relative"
+      //크기 및 여백
+      w="500px"
+      h="360px"
+      //배경
+      borderRadius="xl"
+      boxShadow="0 0 10px #00A29D"
+      overflow="hidden"
+    >
       {/* 상단 */}
-      <Box
-        //위치
-        position="absolute"
+      <Flex
         //크기 및 여백
         w="100%"
         h="50%"
-        mt="0px"
         //기능
         as="button"
         onClick={handleClick}
@@ -39,17 +46,15 @@ const VC = ({ vcObj }) => {
           //크기
           w="100%"
           h="100%"
-          //배경
-          objectFit="cover"
           //효과
           _hover={{
             transform: "scale(1.1)",
             transition: "transform 0.3s ease",
           }}
         />
-      </Box>
+      </Flex>
       {/* 하단 */}
-      <Box
+      <Flex
         //위치
         position="absolute"
         //크기 및 여백
@@ -61,82 +66,80 @@ const VC = ({ vcObj }) => {
         <Image
           //사진 위치
           src={vcObj.vc_logo}
-          //위치
-          position="absolute"
           //크기 및 여백
           w="80px"
           h="80px"
           mt="-40px"
           ml="20px"
           //배경
-          borderRadius="20%"
+          borderRadius="2xl"
         />
         {/* VC 이름 */}
         <Heading
           //위치
           position="absolute"
           //여백
-          mt="40px"
+          mt="45px"
           ml="20px"
           //글자
-          as="h1"
           size="lg"
         >
           {vcObj.vc_name}
         </Heading>
+
         {/* VC 정보 */}
         <Text
           //위치
           position="absolute"
           //여백
-          mt="80px"
-          ml="20px"
-          mr="20px"
+          mt="85px"
+          marginX="20px"
           //글자
           fontSize="md"
           noOfLines={[1, 2]}
         >
           {vcObj.vc_info}
         </Text>
+
         {/* VC 카테고리 */}
         <Box
           //위치
           position="absolute"
           //여백
+          w="auto"
           p="5px 10px"
-          mt="5px"
+          mt="10px"
           ml="390px"
           //배경
           bg="#00A29D"
           borderRadius="xl"
           //글자
-          as="b"
           fontSize="xs"
           color="white"
         >
           {vcObj.vc_category}
         </Box>
+
         {/* VC INFO 이동버튼 */}
         <Button
           //위치
           position="absolute"
           //여백
-          mt="45px"
+          mt="50px"
           ml="390px"
           //배경
-          bg="none"
           variant="link"
           //글자
-          as="b"
-          fontSize="md"
           color="black"
           onClick={() => handleClick()}
         >
           자세히 보기 {">"}
         </Button>
-      </Box>
-    </Box>
+      </Flex>
+    </Flex>
   );
 };
 
 export default VC;
+
+//23.07.21 1차 코드 수정 완료
