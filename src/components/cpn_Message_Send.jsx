7faf35f797bef2f2a0e052388fabc5968717111e
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-21 10:36:56 +0900

    cpn_Message_Send 코드정리

--- src/components/cpn_Message_Send.jsx
+++ src/components/cpn_Message_Send.jsx
@@ -2,19 +2,14 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useState } from "react";
 import { auth, db } from "../firebase-config";
 import { useLocation } from "react-router-dom";
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  Input,
-  IconButton,
-} from "@chakra-ui/react";
-import { FaSmile, FaTimesCircle } from "react-icons/fa";
+import { Button, Flex, FormControl, Input } from "@chakra-ui/react";
+// import { IconButton } from "@chakra-ui/react";
+
+// import { FaSmile } from "react-icons/fa";
 
 const MessageSend = ({ scroll }) => {
   const [input, setInput] = useState("");
-  const [open, setOpen] = useState("close");
+  // const [open, setOpen] = useState("close");
 
   const { state } = useLocation();
 
@@ -39,26 +34,37 @@ const MessageSend = ({ scroll }) => {
     scroll.current.scrollIntoView({ behavior: "smooth" });
   };
 
-  const toggleEmojiPicker = () => {
-    setOpen(!open);
-  };
+  // const toggleEmojiPicker = () => {
+  //   setOpen(!open);
+  // };
 
   return (
-    <Flex as="form" onSubmit={sendMessage} alignItems="center">
+    <Flex
+      //정렬
+      alignItems="center"
+      //기능
+      as="form"
+      onSubmit={sendMessage}
+    >
       {/* <IconButton
+        //크기 및 여백
         maxW="100px"
+        mr={2}
+        //배경
+        colorScheme="teal"
+        variant="outline"
+        //글자
+        fontSize="20px"
+        //기능
         type="button"
         onClick={toggleEmojiPicker}
         aria-label="Emoji Picker"
         icon={<FaSmile />}
-        fontSize="20px"
-        mr={2}
-        colorScheme="teal"
-        variant="outline"
       /> */}
 
       <FormControl flex={1}>
         <Input
+          //기능
           type="text"
           placeholder="Enter your message here"
           value={input}
@@ -66,7 +72,15 @@ const MessageSend = ({ scroll }) => {
         />
       </FormControl>
 
-      <Button maxW="200px" type="submit" ml={2} colorScheme="teal">
+      <Button
+        //크기 및 여백
+        maxW="200px"
+        ml={2}
+        //배경
+        colorScheme="teal"
+        //기능
+        type="submit"
+      >
         Send
       </Button>
     </Flex>
