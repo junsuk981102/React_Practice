b0282efb7afc65a5658687bad41bf68f54718bb8
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-26 15:19:44 +0900

    커뮤니티 컴포넌트 수정

--- src/screens/community/screen_Community_Info.js
+++ src/screens/community/screen_Community_Info.js
@@ -3,12 +3,16 @@ import { useNavigate, useLocation } from "react-router-dom";
 import ErrorModal from "../../components/community/cpn_Error_Modal";
 import ComInfoBasic from "../../components/community/cpn_Com_Info_Basic";
 import ComInfoCurrent from "../../components/community/cpn_Com_Info_Current";
-import ComInfoEnter from "../../components/community/cpn_Com_Info_Enter";
 import ComInfoFavor from "../../components/community/cpn_Com_Info_Favor";
 import ComInfoInvest from "../../components/community/cpn_Com_Info_Invest";
 import ComInfoTicket from "../../components/community/cpn_Com_Info_Ticket";
-
-import { Box, Heading, useBreakpointValue, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  useBreakpointValue,
+  Flex,
+} from "@chakra-ui/react";
 
 const ScreenRoomInfo = () => {
   const { state } = useLocation();
@@ -122,10 +126,21 @@ const ScreenRoomInfo = () => {
           <ComInfoCurrent state={state} />
 
           {/* 커뮤니티 참여 섹션 */}
-          <ComInfoEnter
-            ownerCount={ownerCount}
-            handleClick_chat={handleClick_chat}
-          />
+          <Flex justifyContent="center" p="20px">
+            <Button
+              w="600px"
+              h="60px"
+              bg={ownerCount > 0 ? "#00A29D" : "grey"}
+              borderRadius="xl"
+              variant="none"
+              fontSize="lg"
+              fontWeight="bold"
+              color="white"
+              onClick={handleClick_chat}
+            >
+              커뮤니티 참여하기
+            </Button>
+          </Flex>
         </Box>
       </Box>
 
