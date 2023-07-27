86a34bab22b80f2ca2013545697f0bda6c4ef603
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:48:22 +0900

    Update cpn_Community_Category.js
    23.07.27 1차 코드 수정

--- src/components/community/cpn_Community_Category.js
+++ src/components/community/cpn_Community_Category.js
@@ -4,150 +4,120 @@ import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 const CommunityCategory = ({ selectedValue, handleMenuItemClick }) => {
   return (
     <Menu>
+      {/* 커뮤니티 카테고리 버튼 */}
       <MenuButton
-        w="100px"
-        h="40px"
+        p="5px 20px"
         bg="#00A29D"
         borderRadius="3xl"
-        fontSize="md"
         fontWeight="bold"
         color="white"
       >
         {selectedValue}
       </MenuButton>
+      {/* 커뮤니티 카테고리 리스트 */}
       <MenuList bg="#00A29D" color="white">
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("전체")}
         >
           전체
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("핀테크")}
         >
           핀테크
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("금융")}
         >
           금융
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("반려동물")}
         >
           반려동물
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("VR")}
         >
           VR
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("AR")}
         >
           AR
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("ESG")}
         >
           ESG
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("전기차")}
         >
           전기차
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("블록체인")}
         >
           블록체인
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("음악")}
         >
           음악
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("미술")}
         >
           미술
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("자동차")}
         >
           자동차
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("신기술")}
         >
           신기술
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("F&B")}
         >
           F&B
         </MenuItem>
         <MenuItem
-          //배경
           bg="none"
           borderTop="1px solid white"
           borderBottom="1px solid white"
-          //기능
           onClick={() => handleMenuItemClick("헬스케어")}
         >
           헬스케어
@@ -158,3 +128,5 @@ const CommunityCategory = ({ selectedValue, handleMenuItemClick }) => {
 };
 
 export default CommunityCategory;
+
+//23.07.27 1차 코드 수정
