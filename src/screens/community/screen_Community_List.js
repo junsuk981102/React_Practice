8e917efc61bf8ef25ffac12d8c05761e420a32c4
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:50:14 +0900

    Update screen_Community_List.js
    23.07.27 2차 코드 수정

--- src/screens/community/screen_Community_List.js
+++ src/screens/community/screen_Community_List.js
@@ -2,16 +2,12 @@ import React, { useState, useEffect } from "react";
 import { dbService } from "../../firebase-config";
 import CommunityCategory from "../../components/community/cpn_Community_Category";
 import CommunityGridlist from "../../components/community/cpn_Community_Gridlist";
-
-import { Heading, Box, useBreakpointValue, Flex } from "@chakra-ui/react";
+import { Flex, Heading, useBreakpointValue } from "@chakra-ui/react";
 
 const ScreenRoomList = () => {
-  //커뮤니티 설정
-  const [communities, setCommunities] = useState([]);
-  //양쪽 여백
-  const boxPadding = useBreakpointValue({ base: "20px", xl: "200px" });
-  //카테고리 리스트 초기값 설정
-  const [selectedValue, setSelectedValue] = useState("전체");
+  const [communities, setCommunities] = useState([]); //커뮤니티 설정
+  const boxPadding = useBreakpointValue({ base: "20px", xl: "200px" }); //양쪽 여백
+  const [selectedValue, setSelectedValue] = useState("전체"); //카테고리 리스트 초기값 설정
   //카테고리 리스트 설정값 변경
   const handleMenuItemClick = (value) => {
     setSelectedValue(value);
@@ -29,47 +25,39 @@ const ScreenRoomList = () => {
   }, []);
 
   return (
-    //커뮤니티 리스트 전체 화면
+    <>
+      {/* 커뮤니티 리스트 전체 화면 */}
 
-    //커뮤니티 리스트 전체 배경 화면
-    <Box
-      //크기 및 여백
-      h="auto"
-      px={boxPadding}
-      pb="200px"
-      //배경
-      bg="#E5F2F2"
-      borderTop="1px solid #00A29D"
-    >
-      {/* 커뮤니티 제목 & 카테고리 버튼 */}
+      {/* 커뮤니티 리스트 전체 배경 화면 */}
       <Flex
-        //정렬
-        justifyContent="space-between"
-        //여백
-        marginY="30px"
+        flexDirection="column"
+        h="auto"
+        px={boxPadding}
+        pb="200px"
+        bg="#E5F2F2"
+        borderTop="1px solid #00A29D"
       >
-        {/* 커뮤니티 제목 */}
-        <Heading
-          //글자
-          size="lg"
-        >
-          커뮤니티
-        </Heading>
-        {/* 카테고리 버튼 */}
-        <CommunityCategory
+        {/* 커뮤니티 제목 & 카테고리 버튼 */}
+        <Flex justifyContent="space-between" m="30px 0">
+          {/* 커뮤니티 제목 */}
+          <Heading size="lg">커뮤니티</Heading>
+          {/* 카테고리 버튼 */}
+          <CommunityCategory
+            selectedValue={selectedValue}
+            handleMenuItemClick={handleMenuItemClick}
+          />
+        </Flex>
+        {/* 커뮤니티 리스트 섹션 */}
+        <CommunityGridlist
+          communities={communities}
           selectedValue={selectedValue}
-          handleMenuItemClick={handleMenuItemClick}
         />
       </Flex>
-      {/* 커뮤니티 리스트 섹션 */}
-      <CommunityGridlist
-        communities={communities}
-        selectedValue={selectedValue}
-      />
-    </Box>
+    </>
   );
 };
 
 export default ScreenRoomList;
 
-//23.07.24 1차 코드 수정 완료
+//23.07.24 1차 코드 수정
+//23.07.27 2차 코드 수정
