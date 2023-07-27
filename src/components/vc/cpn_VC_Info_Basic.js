cde4eba1de7d558394a8408332e04b6fb6e84752
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 17:25:23 +0900

    Update cpn_VC_Info_Basic.js
    23.07.27 1차 코드 수정 완료

--- src/components/vc/cpn_VC_Info_Basic.js
+++ src/components/vc/cpn_VC_Info_Basic.js
@@ -1,35 +1,43 @@
 import React from "react";
-import { Flex, Heading, Image, Box, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text, Image } from "@chakra-ui/react";
 
 const VCInfoBasic = ({ state }) => {
   return (
-    <Flex alignItems="center" h="auto">
-      <Image
-        src={state.vc_logo}
-        w="200px"
-        h="200px"
-        borderRadius="xl"
-        objectFit="cover"
-      />
-      <Flex flexDirection="column" ml="25px">
-        <Heading size="lg">{state.vc_name}</Heading>
-        <Box
-          textAlign="center"
-          w="100px"
-          p="5px"
-          marginY="10px"
-          bg="#00A29D"
+    <>
+      {/* VC 기본정보 */}
+      <Flex alignItems="center" h="auto">
+        {/* VC 로고 */}
+        <Image
+          src={state.vc_logo}
+          w="200px"
+          h="200px"
           borderRadius="xl"
-          fontWeight="bold"
-          fontSize="md"
-          color="white"
-        >
-          {state.vc_category}{" "}
-        </Box>
-        <Text fontSize="md">{state.vc_info}</Text>
+          objectFit="cover"
+        />
+        <Flex flexDirection="column" ml="25px">
+          {/* 스타트업 이름 */}
+          <Heading size="lg">{state.vc_name}</Heading>
+          {/* 스타트업 카테고리 */}
+          <Flex
+            justifyContent="center"
+            w="100px"
+            p="5px"
+            m="10px 0"
+            bg="#00A29D"
+            borderRadius="xl"
+            fontSize="sm"
+            color="white"
+          >
+            {state.vc_category}
+          </Flex>
+          {/* 스타트업 설명 */}
+          <Text>{state.vc_info}</Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
 export default VCInfoBasic;
+
+//23.07.27 1차 코드 수정
