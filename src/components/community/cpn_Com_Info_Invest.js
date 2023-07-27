ad15e51b7c74a321f1de874298bc33eb03c9e780
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:48:02 +0900

    Update cpn_Com_Info_Invest.js
    23.07.27 1차 코드 수정

--- src/components/community/cpn_Com_Info_Invest.js
+++ src/components/community/cpn_Com_Info_Invest.js
@@ -1,57 +1,47 @@
 import React from "react";
-import { Text, Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 const ComInfoInvest = ({ state }) => {
+  //금액 표시 형식 변경(ex.10000->10,000)
   function NumberFormat({ number }) {
     return <span>{number.toLocaleString()}</span>;
   }
 
   return (
-    <Box mt="30px" ml="20px">
-      <Text mr="20px" fontWeight="bold" fontSize="lg">
+    <Box m="30px 0 0 20px">
+      <Text m="0 20px 0 0" fontSize="lg" fontWeight="bold">
         투자 목표 금액
       </Text>
-      <Text fontWeight="bold" fontSize="lg" color="#00A29D">
+      {/* 커뮤니티 투자 달성 현황 */}
+      <Text fontSize="lg" fontWeight="bold" color="#00A29D">
         {(state.com_now_investment / state.com_total_investment) * 100}% 달성
       </Text>
       {/* 커뮤니티 투자 목표 금액 그래프 */}
-      <Box position="relative" mt="20px">
+      <Box position="relative" m="20px 0 0 0">
         {/* 목표 금액 그래프 */}
         <Box
-          //위치 및 정렬
           position="absolute"
-          top="0"
-          left="0"
-          //크기
           w="90%"
           h="45px"
-          //배경
           bg="white"
           border="1px solid #00A29D"
           borderRadius="3xl"
         />
         {/* 현재 금액 그래프 */}
         <Box
-          //위치 및 정렬
           position="absolute"
-          top="0"
-          left="0"
-          //크기
           w={(state.com_now_investment / state.com_total_investment) * 90 + "%"}
           h="45px"
-          //배경
           bg={"linear-gradient(to right, #00A29D, #FFFFFF)"}
           border="1px solid #00A29D"
           borderRadius="3xl"
         />
       </Box>
-      {/* 투자 목표 금액 텍스트 */}
+      {/* 커뮤니티 투자 목표 금액 텍스트 */}
       <Box position="relative" color="#00A29D">
         {/* 목표 금액 텍스트 */}
         <Box position="absolute" top="55" left="85%">
-          <Text as="b">
-            <NumberFormat number={state.com_total_investment} />원
-          </Text>
+          <NumberFormat number={state.com_total_investment} />원
         </Box>
         {/* 현재 금액 텍스트 */}
         <Box
@@ -61,9 +51,7 @@ const ComInfoInvest = ({ state }) => {
             (state.com_now_investment / state.com_total_investment) * 80 + "%"
           }
         >
-          <Text fontWeight="bold">
-            <NumberFormat number={state.com_now_investment} />원
-          </Text>
+          <NumberFormat number={state.com_now_investment} />원
         </Box>
       </Box>
     </Box>
@@ -71,3 +59,5 @@ const ComInfoInvest = ({ state }) => {
 };
 
 export default ComInfoInvest;
+
+//23.07.27 1차 코드 수정
