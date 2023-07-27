1401216fa37037226cd6417dfe8e274b3c305fd0
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 14:27:08 +0900

    Update cpn_Startup_Card.js
    23.07.27 1차 코드 수정

--- src/components/startup/cpn_Startup_Card.js
+++ src/components/startup/cpn_Startup_Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Image, Button, Heading, Text, Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Button, Image } from "@chakra-ui/react";
 
 const StartupCard = ({ startupObj }) => {
   const navi = useNavigate();
-
+  //스타트업 정보 페이지로 정보 전송
   function handleClick() {
     navi(`/screen_startup_info`, {
       state: {
@@ -20,33 +20,21 @@ const StartupCard = ({ startupObj }) => {
 
   return (
     <Flex
-      //정렬
       position="relative"
-      //크기 및 여백
       w="500px"
       h="360px"
-      //배경
+      bg="white"
       borderRadius="xl"
       boxShadow="0 0 10px #00A29D"
       overflow="hidden"
     >
       {/* 상단 */}
-      <Flex
-        //크기 및 여백
-        w="100%"
-        h="50%"
-        //기능
-        as="button"
-        onClick={handleClick}
-      >
+      <Flex w="100%" h="50%" as="button" onClick={handleClick}>
         {/* 스타트업 배경 사진 */}
         <Image
-          //사진 위치
           src={startupObj.sup_backgroundImg}
-          //크기
           w="100%"
           h="100%"
-          //효과
           _hover={{
             transform: "scale(1.1)",
             transition: "transform 0.3s ease",
@@ -54,82 +42,40 @@ const StartupCard = ({ startupObj }) => {
         />
       </Flex>
       {/* 하단 */}
-      <Flex
-        //위치
-        position="absolute"
-        //크기 및 여백
-        w="100%"
-        h="50%"
-        mt="180px"
-      >
+      <Flex position="absolute" w="100%" mt="180px">
         {/* 스타트업 로고 */}
         <Image
-          //사진 위치
           src={startupObj.sup_logo}
-          //크기 및 여백
           w="80px"
           h="80px"
-          mt="-40px"
-          ml="20px"
-          //배경
+          m="-40px 0 0 20px"
           borderRadius="2xl"
         />
         {/* 스타트업 이름 */}
-        <Heading
-          //위치
-          position="absolute"
-          //여백
-          mt="45px"
-          ml="20px"
-          //글자
-          size="lg"
-        >
+        <Heading position="absolute" m="45px 0 0 20px" size="lg">
           {startupObj.sup_name}
         </Heading>
-
         {/* 스타트업 정보 */}
-        <Text
-          //위치
-          position="absolute"
-          //여백
-          mt="85px"
-          marginX="20px"
-          //글자
-          fontSize="md"
-          noOfLines={[1, 2]}
-        >
+        <Text position="absolute" m="85px 20px 0 20px" noOfLines={[1, 2]}>
           {startupObj.sup_info}
         </Text>
-
         {/* 스타트업 카테고리 */}
         <Box
-          //위치
           position="absolute"
-          //여백
-          w="auto"
           p="5px 10px"
-          mt="10px"
-          ml="390px"
-          //배경
+          m="10px 0 0 390px"
           bg="#00A29D"
           borderRadius="xl"
-          //글자
           fontSize="xs"
           color="white"
         >
           {startupObj.sup_category}
         </Box>
-
         {/* 스타트업 INFO 이동버튼 */}
         <Button
-          //위치
           position="absolute"
-          //여백
-          mt="50px"
-          ml="390px"
-          //배경
+          m="50px 0 0 390px"
           variant="link"
-          //글자
           color="black"
           onClick={() => handleClick()}
         >
@@ -143,3 +89,4 @@ const StartupCard = ({ startupObj }) => {
 export default StartupCard;
 
 //23.07.21 1차 코드 수정 완료
+//23.07.27 2차 코드 수정 완료
