6df22d51907c7a6768f1dd5cbe31c8074488ce6f
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:49:49 +0900

    Update cpn_Thr_Second_Voting_After.js

--- src/components/community/cpn_Thr_Second_Voting_After.js
+++ src/components/community/cpn_Thr_Second_Voting_After.js
@@ -1,25 +1,24 @@
 import React from "react";
-
-import { Text, Flex, Image } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
 
 const ThrFirstVotingAft = ({ state }) => {
-  const percentyes = 642;
-  const percentno = 424;
-  const percentyesorno = percentyes + percentno;
+  const percentyes = 642; // 찬성 득표
+  const percentno = 424; // 반대 득표
+  const percentyesorno = percentyes + percentno; // 총 득표
 
   return (
     <>
       {/* 정보 */}
-      <Flex flexDirection="column" p="10px" mb="30px">
+      <Flex flexDirection="column" p="10px">
         {/* 제목 */}
-        <Flex mb="15px">
-          <Text fontSize="2xl" fontWeight="bold" mr="15px">
+        <Flex m="0 0 15px 0">
+          <Text fontSize="2xl" fontWeight="bold" m="0 15px 0 0">
             2차 투표 종료
           </Text>
           <Image src="../image/community/icon_voting.png" w="30px" h="30px" />
         </Flex>
         {/* 설명 */}
-        <Text fontSize="xl" fontWeight="bold" mb="15px">
+        <Text m="0 0 15px 0">
           {state.com_name} 2차 투표가 모두 종료되었습니다. <br />
           투표 결과에 따라, 1주일 이내에 {"["}펫프렌즈{"]"}에 대한 투자 계약이
           시작됩니다.
@@ -29,13 +28,13 @@ const ThrFirstVotingAft = ({ state }) => {
           <Text m="25px" fontSize="lg" fontWeight="bold">
             {"<"}투표 결과{">"}
           </Text>
-          <Flex flexDirection="column" textAlign="center" marginX="40px">
+          <Flex flexDirection="column" textAlign="center">
             <Image
               src="../image/community/company_ex1.png"
               w="250px"
               h="250px"
             />
-            <Text fontSize="2xl" fontWeight="bold" marginY="5px">
+            <Text fontSize="2xl" fontWeight="bold" m="5px 0">
               펫프렌즈
             </Text>
             <Text fontSize="2xl" fontWeight="bold" color="#00A29D">
@@ -49,3 +48,5 @@ const ThrFirstVotingAft = ({ state }) => {
 };
 
 export default ThrFirstVotingAft;
+
+//23.07.27 1차 코드 수정
