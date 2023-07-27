339c704c0880055d0f3575776a5d0d5189dcf52c
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:50:19 +0900

    Update screen_Community_Room.js
    23.07.27 1차 코드 수정

--- src/screens/community/screen_Community_Room.js
+++ src/screens/community/screen_Community_Room.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
-import { Flex, Heading } from "@chakra-ui/react";
-
-import ThrTab from "../../components/community/cpn_Thr_Tab";
 import ThrComInfo from "../../components/community/cpn_Thr_Com_Info";
+import ThrTab from "../../components/community/cpn_Thr_Tab";
+import { Flex } from "@chakra-ui/react";
 
 function ScreenChat() {
   const { state } = useLocation();
@@ -24,38 +23,26 @@ function ScreenChat() {
 
       {/* 커뮤니티 전체 화면 배경 */}
       <Flex
-        //정렬
         flexDirection="column"
         alignItems="center"
-        //크기 및 여백
         h="auto"
         pb="200px"
-        //배경
         bg="#E5F2F2"
         borderTop="1px solid #00A29D"
       >
-        {/* 커뮤니티 이름 */}
-        <Heading
-          //크기 및 여백
-          size="md"
-          mt="30px"
-          mb="30px"
-        >
-          {state.com_name}
-        </Heading>
         {/* 커뮤니티 구조 */}
         <Flex
           flexDirection="column"
           w="750px"
           h="auto"
           maxH="1500px"
-          pb="50px"
+          m="60px 0 0 0"
+          p="0 0 50px 0"
           bg="white"
           border="1px solid black"
         >
           {/* 커뮤니티 정보 섹션 */}
           <ThrComInfo state={state} comCategory={comCategory} />
-
           {/* 커뮤니티 Tab */}
           <ThrTab
             state={state}
@@ -69,3 +56,5 @@ function ScreenChat() {
 }
 
 export default ScreenChat;
+
+//23.07.27 1차 코드 수정
