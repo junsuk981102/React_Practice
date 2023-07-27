a38608e4a8624300c26e62ef3c315e3c7148ed3e
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 14:27:23 +0900

    Update cpn_Startup_Info_Company.js
    23.07.27 1차 코드 수정

--- src/components/startup/cpn_Startup_Info_Company.js
+++ src/components/startup/cpn_Startup_Info_Company.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { dbService } from "../../firebase-config";
-import { Text, Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 const StartupInfoCompany = ({
   state,
@@ -34,37 +34,43 @@ const StartupInfoCompany = ({
   }, [state.id]);
 
   return (
-    <Flex flexDirection="column" m="20px">
-      <Text fontWeight="bold" fontSize="lg">
-        기업정보
-      </Text>
-      <Flex m="15px">
-        <Flex flexDirection="column" color="grey">
-          <Text mb="10px">대표자</Text>
-          <Text mb="10px">국적</Text>
-          <Text>회사구분</Text>
-        </Flex>
-        <Flex flexDirection="column" ml="50px">
-          <Text mb="10px">{sup_ceo}</Text>
-          <Text mb="10px">{sup_nationality}</Text>
-          <Text>{sup_size}</Text>
-        </Flex>
-
-        <Flex>
-          <Flex flexDirection="column" ml="100px" color="grey">
-            <Text mb="10px">발행주식</Text>
-            <Text mb="10px">업종</Text>
-            <Text>홈페이지</Text>
+    <>
+      {/* 스타트업 기업 정보 */}
+      <Flex flexDirection="column" m="20px">
+        <Text fontWeight="bold" fontSize="lg">
+          기업정보
+        </Text>
+        <Flex m="15px">
+          {/* 왼쪽 섹션 */}
+          <Flex flexDirection="column" color="grey">
+            <Text mb="10px">대표자</Text>
+            <Text mb="10px">국적</Text>
+            <Text>회사구분</Text>
           </Flex>
           <Flex flexDirection="column" ml="50px">
-            <Text mb="10px">통일 주권</Text>
-            <Text mb="10px">데이터베이스 및 온라인 정보 제공업</Text>
-            <Text>{sup_homepage}</Text>
+            <Text mb="10px">{sup_ceo}</Text>
+            <Text mb="10px">{sup_nationality}</Text>
+            <Text>{sup_size}</Text>
+          </Flex>
+          {/* 오른쪽 섹션 */}
+          <Flex>
+            <Flex flexDirection="column" ml="100px" color="grey">
+              <Text mb="10px">발행주식</Text>
+              <Text mb="10px">업종</Text>
+              <Text>홈페이지</Text>
+            </Flex>
+            <Flex flexDirection="column" ml="50px">
+              <Text mb="10px">통일 주권</Text>
+              <Text mb="10px">데이터베이스 및 온라인 정보 제공업</Text>
+              <Text>{sup_homepage}</Text>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
 export default StartupInfoCompany;
+
+//23.07.27 1차 코드 수정
