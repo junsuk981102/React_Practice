eab03a261fe7d29d0cbe618961a879188e2adab4
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 14:27:29 +0900

    Update cpn_Startup_Info_Product.js
    23.07.27 1차 코드 수정

--- src/components/startup/cpn_Startup_Info_Product.js
+++ src/components/startup/cpn_Startup_Info_Product.js
@@ -1,29 +1,34 @@
 import React from "react";
-import { Text, Image, Flex } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
 
 const StartupInfoProduct = ({ state }) => {
   return (
-    <Flex flexDirection="column" m="20px">
-      <Text fontWeight="bold" fontSize="lg">
-        서비스/제품
-      </Text>
-      <Flex m="15px">
-        <Image
-          src={state.sup_logo}
-          w="60px"
-          h="60px"
-          borderRadius="3xl"
-          objectFit="cover"
-        />
-        <Flex flexDirection="column" m="7px">
-          <Text fontSize="lg">{state.sup_name}</Text>
-          <Text fontSize="sm" color="#00A29D">
-            간편 모바일 반려동물등록 서비스
-          </Text>
+    <>
+      {/* 스타트업 서비스/제품 */}
+      <Flex flexDirection="column" m="20px">
+        <Text fontWeight="bold" fontSize="lg">
+          서비스/제품
+        </Text>
+        <Flex m="15px">
+          <Image
+            src={state.sup_logo}
+            w="60px"
+            h="60px"
+            borderRadius="3xl"
+            objectFit="cover"
+          />
+          <Flex flexDirection="column" m="7px">
+            <Text fontSize="lg">{state.sup_name}</Text>
+            <Text fontSize="sm" color="#00A29D">
+              간편 모바일 반려동물등록 서비스
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
 export default StartupInfoProduct;
+
+//23.07.27 1차 코드 수정
