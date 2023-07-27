5305082c290fa1a60afd0b2af0f8135b6caf041f
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 12:48:31 +0900

    Update cpn_Error_Modal.js
    23.07.27 1차 코드 수정

--- src/components/community/cpn_Error_Modal.js
+++ src/components/community/cpn_Error_Modal.js
@@ -10,14 +10,15 @@ import {
 
 const ErrorModal = ({ isOpen, onClose }) => {
   return (
+    // 커뮤니티 입장 에러 메시지
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalBody mt="20px" fontSize="xl" fontWeight="bold">
+        <ModalBody m="20px 0 0 0" fontSize="xl" fontWeight="bold">
           커뮤니티에 입장하기 위해 티켓을 구매해주세요.
         </ModalBody>
         <ModalFooter>
-          <Button mr={3} bg="#00A29D" onClick={onClose}>
+          <Button m="0 5px 0 0" bg="#00A29D" onClick={onClose}>
             Close
           </Button>
         </ModalFooter>
@@ -27,3 +28,5 @@ const ErrorModal = ({ isOpen, onClose }) => {
 };
 
 export default ErrorModal;
+
+//23.07.27 1차 코드 수정
