2cafe27baa6387a579b0b1d6aff7eef54095f761
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 14:27:26 +0900

    Update cpn_Startup_Info_Invest.js
    23.07.27 1차 코드 수정

--- src/components/startup/cpn_Startup_Info_Invest.js
+++ src/components/startup/cpn_Startup_Info_Invest.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { dbService } from "../../firebase-config";
-import { Text, Flex, Image } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
 
 const StartupInfoInvest = ({
   state,
@@ -33,92 +33,94 @@ const StartupInfoInvest = ({
   }, [state.id]);
 
   return (
-    <Flex m="20px">
-      <Flex flexDirection="column" mr="50px">
-        <Text fontWeight="bold" fontSize="lg">
-          투자정보
-        </Text>
-        <Flex m="15px">
-          <Flex flexDirection="column" color="grey">
-            <Text mb="10px">최종투자 단계</Text>
-            <Text mb="10px">누적투자 금액</Text>
-            <Text>투자유치 횟수</Text>
+    <>
+      {/* 스타트업 투자 정보 */}
+      <Flex m="20px">
+        {/* 스타트업 투자 정보 섹션 */}
+        <Flex flexDirection="column" mr="50px">
+          <Text fontWeight="bold" fontSize="lg">
+            투자정보
+          </Text>
+          <Flex m="15px">
+            <Flex flexDirection="column" color="grey">
+              <Text mb="10px">최종투자 단계</Text>
+              <Text mb="10px">누적투자 금액</Text>
+              <Text>투자유치 횟수</Text>
+            </Flex>
+            <Flex flexDirection="column" ml="50px">
+              <Text mb="10px">{sup_roundOfInvestment}</Text>
+              <Text mb="10px">{sup_amountOfInvestment}</Text>
+              <Text>{sup_numOfInvestment}</Text>
+            </Flex>
           </Flex>
-          <Flex flexDirection="column" ml="50px">
-            <Text mb="10px">{sup_roundOfInvestment}</Text>
-            <Text mb="10px">{sup_amountOfInvestment}</Text>
-            <Text>{sup_numOfInvestment}</Text>
+        </Flex>
+        {/* 스타트업 투자자 섹션 */}
+        <Flex flexDirection="column">
+          <Text fontWeight="bold" fontSize="lg">
+            투자자
+          </Text>
+          <Flex mt="15px">
+            <Flex
+              flexDirection="column"
+              alignItems="center"
+              m="0 10px"
+              p="5px"
+              borderRadius="xl"
+              boxShadow="0 0 5px #00A29D"
+            >
+              <Image
+                src="../image/community/company_ex1.png"
+                w="100px"
+                h="100px"
+                borderRadius="xl"
+              />
+              <Text mt="5px" fontSize="sm">
+                펫프랜즈
+              </Text>
+            </Flex>
+            <Flex
+              flexDirection="column"
+              alignItems="center"
+              m="0 10px"
+              p="5px"
+              borderRadius="xl"
+              boxShadow="0 0 5px #00A29D"
+            >
+              <Image
+                src="../image/community/company_ex2.png"
+                w="100px"
+                h="100px"
+                borderRadius="xl"
+              />
+              <Text mt="5px" fontSize="sm">
+                페오펫
+              </Text>
+            </Flex>
+            <Flex
+              flexDirection="column"
+              alignItems="center"
+              m="0 10px"
+              p="5px"
+              borderRadius="xl"
+              boxShadow="0 0 5px #00A29D"
+            >
+              <Image
+                src="../image/community/company_ex3.png"
+                w="100px"
+                h="100px"
+                borderRadius="xl"
+              />
+              <Text mt="5px" fontSize="sm">
+                핏펫
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-
-      <Flex flexDirection="column">
-        <Text
-          //글자
-          fontWeight="bold"
-          fontSize="lg"
-        >
-          투자자
-        </Text>
-        <Flex mt="15px">
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            marginX="10px"
-            p="5px"
-            borderRadius="xl"
-            boxShadow="0 0 5px #00A29D"
-          >
-            <Image
-              src="../image/community/company_ex1.png"
-              w="100px"
-              h="100px"
-              borderRadius="xl"
-            />
-            <Text mt="5px" fontSize="sm">
-              펫프랜즈
-            </Text>
-          </Flex>
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            marginX="20px"
-            p="5px"
-            borderRadius="xl"
-            boxShadow="0 0 5px #00A29D"
-          >
-            <Image
-              src="../image/community/company_ex2.png"
-              w="100px"
-              h="100px"
-              borderRadius="xl"
-            />
-            <Text mt="5px" fontSize="sm">
-              페오펫
-            </Text>
-          </Flex>
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            marginX="10px"
-            p="5px"
-            borderRadius="xl"
-            boxShadow="0 0 5px #00A29D"
-          >
-            <Image
-              src="../image/community/company_ex3.png"
-              w="100px"
-              h="100px"
-              borderRadius="xl"
-            />
-            <Text mt="5px" fontSize="sm">
-              핏펫
-            </Text>
-          </Flex>
-        </Flex>
-      </Flex>
-    </Flex>
+    </>
   );
 };
 
 export default StartupInfoInvest;
+
+//23.07.27 1차 코드 수정
