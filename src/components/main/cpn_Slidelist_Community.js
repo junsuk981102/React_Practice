8447df96f9f97c775134acedc3e210ba8f31772e
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-27 13:26:12 +0900

    Update cpn_Slidelist_Community.js
    23.07.27 1차 코드 수정

--- src/components/main/cpn_Slidelist_Community.js
+++ src/components/main/cpn_Slidelist_Community.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-import { Box } from "@chakra-ui/react";
 import CommunityCard from "../community/cpn_Community_Card";
+import { Box } from "@chakra-ui/react";
 
 // 커뮤니티 리스트 렌더링
 export const renderCommunities = (communities) => {
@@ -13,7 +13,6 @@ export const renderCommunities = (communities) => {
     </Box>
   ));
 };
-
 // 커뮤니티 슬라이드 설정
 const com_slide_settings = {
   dots: true, // 페이지 표시 여부
@@ -24,7 +23,6 @@ const com_slide_settings = {
   autoplay: true, // 자동 전환 설정
   autoplaySpeed: 3000, // 자동전환 속도
 };
-
 // 커뮤니티 슬라이드 리스트
 export const SlideListCom = ({ communities }) => {
   return (
@@ -32,3 +30,5 @@ export const SlideListCom = ({ communities }) => {
     <Slider {...com_slide_settings}>{renderCommunities(communities)}</Slider>
   );
 };
+
+//23.07.27 1차 코드 수정
