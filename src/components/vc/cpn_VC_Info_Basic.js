aa6efc2a3b3c3c43e51997302cc8b2ebcea0bdd5
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-31 11:04:59 +0900

    Update cpn_VC_Info_Basic.js
    즐겨찾기 버튼 추가

--- src/components/vc/cpn_VC_Info_Basic.js
+++ src/components/vc/cpn_VC_Info_Basic.js
@@ -1,7 +1,39 @@
-import React from "react";
-import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { auth, dbService } from "../../firebase-config";
+import { arrayUnion } from "firebase/firestore";
+import { Flex, Heading, Text, Image, IconButton } from "@chakra-ui/react";
+import { FaStar, FaRegStar } from "react-icons/fa6";
 
 const VCInfoBasic = ({ state }) => {
+  const [userUid, setUserUid] = useState("");
+  const [activeButton, setActiveButton] = useState("");
+
+  useEffect(() => {
+    const getUserUid = async () => {
+      try {
+        const user = auth.currentUser;
+        if (user) {
+          setUserUid(user.uid);
+        }
+      } catch (error) {
+        console.log("사용자 UID 가져오기 실패:", error);
+      }
+    };
+
+    getUserUid();
+  }, []);
+
+  function handleClick() {
+    setActiveButton((prevState) => (prevState === "yellow" ? "" : "yellow"));
+    if (userUid) {
+      const vcUid = state.id;
+      const userDocRef = dbService.collection("user_list").doc(userUid);
+      userDocRef.update({
+        vcUids: arrayUnion(vcUid),
+      });
+    }
+  }
+
   return (
     <>
       {/* VC 기본정보 */}
@@ -15,8 +47,24 @@ const VCInfoBasic = ({ state }) => {
           objectFit="cover"
         />
         <Flex flexDirection="column" ml="25px">
-          {/* 스타트업 이름 */}
-          <Heading size="lg">{state.vc_name}</Heading>
+          <Flex alignItems="center">
+            {/* 스타트업 이름 */}
+            <Heading size="lg">{state.vc_name}</Heading>
+            {/* 즐겨찾기 버튼 */}
+            <IconButton
+              aria-label="Like"
+              backgroundColor="white"
+              color={activeButton === "yellow" ? "yellow" : "black"}
+              onClick={handleClick}
+              icon={
+                activeButton === "yellow" ? (
+                  <FaStar style={{ fontSize: "25px" }} />
+                ) : (
+                  <FaRegStar style={{ fontSize: "25px" }} />
+                )
+              }
+            />
+          </Flex>
           {/* 스타트업 카테고리 */}
           <Flex
             justifyContent="center"
