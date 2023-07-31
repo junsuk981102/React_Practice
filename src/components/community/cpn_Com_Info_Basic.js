2713aab4fa019ef5b687c6451f3b3f3de32efdc5
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-31 11:04:51 +0900

    Update cpn_Com_Info_Basic.js
    즐겨찾기 버튼 추가

--- src/components/community/cpn_Com_Info_Basic.js
+++ src/components/community/cpn_Com_Info_Basic.js
@@ -1,7 +1,39 @@
-import React from "react";
-import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { auth, dbService } from "../../firebase-config";
+import { arrayUnion } from "firebase/firestore";
+import { Flex, Heading, Text, Image, IconButton } from "@chakra-ui/react";
+import { FaStar, FaRegStar } from "react-icons/fa6";
 
 const ComInfoBasic = ({ state }) => {
+  const [userUid, setUserUid] = useState("");
+  const [activeButton, setActiveButton] = useState("");
+
+  useEffect(() => {
+    const getUserUid = async () => {
+      try {
+        const user = auth.currentUser;
+        if (user) {
+          setUserUid(user.uid);
+        }
+      } catch (error) {
+        console.log("사용자 UID 가져오기 실패:", error);
+      }
+    };
+
+    getUserUid();
+  }, []);
+
+  function handleClick() {
+    setActiveButton((prevState) => (prevState === "yellow" ? "" : "yellow"));
+    if (userUid) {
+      const communityUid = state.id;
+      const userDocRef = dbService.collection("user_list").doc(userUid);
+      userDocRef.update({
+        communityUids: arrayUnion(communityUid),
+      });
+    }
+  }
+
   //커뮤니티 카테고리 2개 이상일 때 구분
   const comCategory = Array.isArray(state.com_category)
     ? state.com_category
@@ -19,8 +51,24 @@ const ComInfoBasic = ({ state }) => {
       />
       {/* 커뮤니티 기본 정보 */}
       <Flex flexDirection="column" p="25px">
-        {/* 커뮤니티 이름 */}
-        <Heading size="lg">{state.com_name}</Heading>
+        <Flex alignItems="center">
+          {/* 커뮤니티 이름 */}
+          <Heading size="lg">{state.com_name}</Heading>
+          {/* 즐겨찾기 버튼 */}
+          <IconButton
+            aria-label="Like"
+            backgroundColor="white"
+            color={activeButton === "yellow" ? "yellow" : "black"}
+            onClick={handleClick}
+            icon={
+              activeButton === "yellow" ? (
+                <FaStar style={{ fontSize: "25px" }} />
+              ) : (
+                <FaRegStar style={{ fontSize: "25px" }} />
+              )
+            }
+          />
+        </Flex>
         {/* 커뮤니티 카테고리 */}
         <Flex flexDirection="row">
           {comCategory.map((category, index) => (
