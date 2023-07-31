8d21b41f8fc375829434cb4cab38f2c5becded36
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-07-31 11:22:34 +0900

    Update cpn_Startup_Info_Basic.js
    즐겨찾기 추가&삭제 기능, 즐겨찾기 버튼 상태 유지 기능 추가

--- src/components/startup/cpn_Startup_Info_Basic.js
+++ src/components/startup/cpn_Startup_Info_Basic.js
@@ -9,6 +9,11 @@ const StartupInfoBasic = ({ state }) => {
   const [activeButton, setActiveButton] = useState("");
 
   useEffect(() => {
+    const storedActiveButton = localStorage.getItem(`activeButton_${state.id}`);
+    if (storedActiveButton) {
+      setActiveButton(JSON.parse(storedActiveButton)); // 로컬 스토리지에서 가져올 때 문자열을 JSON으로 변환
+    }
+
     const getUserUid = async () => {
       try {
         const user = auth.currentUser;
@@ -21,16 +26,36 @@ const StartupInfoBasic = ({ state }) => {
     };
 
     getUserUid();
-  }, []);
+  }, [state.id]);
 
-  function handleClick() {
-    setActiveButton((prevState) => (prevState === "yellow" ? "" : "yellow"));
+  useEffect(() => {
+    // activeButton 값이 변경될 때 로컬 스토리지에 저장
+    localStorage.setItem(
+      `activeButton_${state.id}`,
+      JSON.stringify(activeButton)
+    ); // 로컬 스토리지에 저장하기 전에 JSON으로 변환
+  }, [activeButton, state.id]);
+
+  async function handleClick() {
     if (userUid) {
       const startupUid = state.id;
       const userDocRef = dbService.collection("user_list").doc(userUid);
-      userDocRef.update({
-        startupUids: arrayUnion(startupUid),
-      });
+      const userDoc = await userDocRef.get();
+      const userStartups = userDoc.data()?.startupUids || [];
+
+      if (userStartups.includes(startupUid)) {
+        // 이미 즐겨찾기에 추가된 상태이면 삭제
+        userDocRef.update({
+          startupUids: userStartups.filter((uid) => uid !== startupUid),
+        });
+        setActiveButton(false);
+      } else {
+        // 즐겨찾기에 추가
+        userDocRef.update({
+          startupUids: arrayUnion(startupUid),
+        });
+        setActiveButton(true);
+      }
     }
   }
 
@@ -54,10 +79,10 @@ const StartupInfoBasic = ({ state }) => {
             <IconButton
               aria-label="Like"
               backgroundColor="white"
-              color={activeButton === "yellow" ? "yellow" : "black"}
+              color={activeButton ? "yellow" : "black"}
               onClick={handleClick}
               icon={
-                activeButton === "yellow" ? (
+                activeButton ? (
                   <FaStar style={{ fontSize: "25px" }} />
                 ) : (
                   <FaRegStar style={{ fontSize: "25px" }} />
@@ -87,5 +112,3 @@ const StartupInfoBasic = ({ state }) => {
 };
 
 export default StartupInfoBasic;
-
-//23.07.27 1차 코드 수정
