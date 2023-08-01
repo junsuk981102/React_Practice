34c368190daeeb99d7c6cd9d83ff176a84c3213d
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-08-01 18:03:29 +0900

    Update screen_Profile.js
    지갑 정보 미출력 오류 수정

--- src/screens/user/screen_Profile.js
+++ src/screens/user/screen_Profile.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import User from "../../components/user/cpn_User";
 import ScreenMyGovernance from "../ticket/screen_My_Governance";
 import { Flex } from "@chakra-ui/react";
 
 const ScreenProfile = () => {
+  const [account, setAccount] = useState(""); // State to store the user's Metamask account address
+
+  const getAccountAddress = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setAccount(accounts[0]);
+        } else {
+          console.error("No account found in Metamask");
+        }
+      } catch (error) {
+        console.error("Error while connecting to Metamask:", error);
+      }
+    } else {
+      console.error("Metamask not detected");
+    }
+  };
+
+  useEffect(() => {
+    // Call the function to get the initial account address
+    getAccountAddress();
+
+    // Subscribe to changes in connected accounts
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setAccount(accounts[0]);
+          window.location.reload();
+        } else {
+          setAccount(""); // If no account available, reset the account state
+        }
+      });
+    }
+  }, []);
+
   return (
     <>
       <Flex
@@ -20,12 +59,10 @@ const ScreenProfile = () => {
         {/* 마이페이지 섹션 */}
         <User />
         {/* 지갑&티켓 섹션 */}
-        <ScreenMyGovernance />
+        <ScreenMyGovernance account={account} />
       </Flex>
     </>
   );
 };
 
 export default ScreenProfile;
-
-//23.07.24 1차 코드 수정 완료
