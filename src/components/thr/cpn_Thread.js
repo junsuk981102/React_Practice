ac58d1989cd77c37858b8e9d7ae9c6f3238a4b18
kswim57 <kswim8914@gmail.com>
2023-08-01 10:06:41 +0900

    스레드 게시물에 사진 추가

--- src/components/thr/cpn_Thread.js
+++ src/components/thr/cpn_Thread.js
@@ -3,13 +3,14 @@ import { deleteDoc, getFirestore, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import { storageService } from "../../firebase-config";
 import { dbService } from "../../firebase-config";
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 const Thread = ({ threadObj }) => {
   const [editing, setEditing] = useState(false);
   return (
     <Box>
       <h4>{threadObj.text}</h4>
+      {threadObj.attachmentUrl && <Image src={threadObj.attachmentUrl} />}
     </Box>
   );
 };
