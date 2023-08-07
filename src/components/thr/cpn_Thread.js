7de80885425dd6e9174bf0deca8915d7dd4d851a
kswim57 <kswim8914@gmail.com>
2023-08-07 09:27:38 +0900

    스레드 수정 기능 추가

--- src/components/thr/cpn_Thread.js
+++ src/components/thr/cpn_Thread.js
@@ -15,7 +15,15 @@ import { deleteObject, ref } from "firebase/storage";
 import { storageService } from "../../firebase-config";
 import { db } from "../../firebase-config";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { Flex, Text, Box, Button, Image, Input } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Box,
+  Button,
+  Image,
+  Input,
+  FormControl,
+} from "@chakra-ui/react";
 import Reply from "./cpn_Replies";
 
 const Thread = ({ threadObj, isOwner, userObj }) => {
@@ -122,126 +130,151 @@ const Thread = ({ threadObj, isOwner, userObj }) => {
         w="100%"
         maxW="700px"
       >
-        {isOpen ? (
+        {editing ? (
           <>
-            <Box onClick={togglePopup} cursor="pointer">
-              <Flex align="center" mb="2">
-                <Image
-                  src={threadObj.creatorPhotoUrl || defaultProfileImage}
-                  w="32px"
-                  h="32px"
-                  rounded="full"
-                  mr="2"
-                  alt="Profile"
-                />
-                <Text fontWeight="bold">{threadObj.creatorName}</Text>
-                <Text ml="2" color="gray.500">
-                  {threadObj.creatorEmail}
-                </Text>
-              </Flex>
-              <Text fontSize="xl" fontWeight="bold" mb="2">
-                {threadObj.text}
-              </Text>
-              {threadObj.attachmentUrl && (
-                <Image src={threadObj.attachmentUrl} mb="2" />
-              )}
-            </Box>
-            {isOwner && (
-              <Flex>
-                <Button onClick={onDelete} mr="2">
-                  Delete
-                </Button>
-                <Button onClick={toggleEditing}>Edit</Button>
-              </Flex>
-            )}
-            <Flex align="center" mt={2}>
-              <Box
-                as="button"
-                onClick={onLike}
-                display="flex"
-                alignItems="center"
-              >
-                {liked ? (
-                  <FaHeart color="red" size={24} />
-                ) : (
-                  <FaRegHeart color="black" size={24} />
-                )}
-                <Text ml="2">{likesCount}</Text>
-              </Box>
-            </Flex>
-            {allReplies.length > 0 && (
-              <Box borderWidth="1px" borderRadius="md" p="4" mt="4">
-                <Flex align="center">
-                  <Image
-                    src={allReplies[0].creatorPhotoUrl || defaultProfileImage}
-                    w="32px"
-                    h="32px"
-                    rounded="full"
-                    mr="2"
-                    alt="Profile"
-                  />
-                  <Text fontWeight="bold">{allReplies[0].creatorName}</Text>
-                  <Text ml="2" color="gray.500">
-                    {allReplies[0].creatorEmail}
-                  </Text>
-                </Flex>
-                <Text ml="2">{allReplies[0].text}</Text>
-              </Box>
-            )}
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Edit your thread"
+                value={newThread}
+                required
+                autoFocus
+                onChange={onChange}
+              />
+              <Button onClick={onSubmit} mt="2">
+                Update thread
+              </Button>
+            </FormControl>
+            <Button onClick={toggleEditing} mt="2">
+              Cancel
+            </Button>
           </>
         ) : (
           <>
-            <Box onClick={togglePopup} cursor="pointer">
-              <Flex align="center" mb="2">
-                <Image
-                  src={threadObj.creatorPhotoUrl || defaultProfileImage}
-                  w="32px"
-                  h="32px"
-                  rounded="full"
-                  mr="2"
-                  alt="Profile"
-                />
-                <Text fontWeight="bold">{threadObj.creatorName}</Text>
-                <Text ml="2" color="gray.500">
-                  {threadObj.creatorEmail}
-                </Text>
-              </Flex>
-              <Text fontSize="xl" fontWeight="bold" mb="2">
-                {threadObj.text}
-              </Text>
-              {threadObj.attachmentUrl && (
-                <Image src={threadObj.attachmentUrl} mb="2" />
-              )}
-            </Box>
-            {isOwner && (
-              <Flex>
-                <Button onClick={onDelete} mr="2">
-                  Delete
-                </Button>
-                <Button onClick={toggleEditing}>Edit</Button>
-              </Flex>
-            )}
-            <Flex align="center" mt={2}>
-              <Box
-                as="button"
-                onClick={onLike}
-                display="flex"
-                alignItems="center"
-              >
-                {liked ? (
-                  <FaHeart color="red" size={24} />
-                ) : (
-                  <FaRegHeart color="black" size={24} />
+            {isOpen ? (
+              <>
+                <Box onClick={togglePopup} cursor="pointer">
+                  <Flex align="center" mb="2">
+                    <Image
+                      src={threadObj.creatorPhotoUrl || defaultProfileImage}
+                      w="32px"
+                      h="32px"
+                      rounded="full"
+                      mr="2"
+                      alt="Profile"
+                    />
+                    <Text fontWeight="bold">{threadObj.creatorName}</Text>
+                    <Text ml="2" color="gray.500">
+                      {threadObj.creatorEmail}
+                    </Text>
+                  </Flex>
+                  <Text fontSize="xl" fontWeight="bold" mb="2">
+                    {threadObj.text}
+                  </Text>
+                  {threadObj.attachmentUrl && (
+                    <Image src={threadObj.attachmentUrl} mb="2" />
+                  )}
+                </Box>
+                {isOwner && (
+                  <Flex>
+                    <Button onClick={onDelete} mr="2">
+                      Delete
+                    </Button>
+                    <Button onClick={toggleEditing}>Edit</Button>
+                  </Flex>
                 )}
-                <Text ml="2">{likesCount}</Text>
-              </Box>
-            </Flex>
-            <Reply
-              threadId={threadObj.id}
-              userObj={userObj}
-              addReply={addReply}
-              replies={allReplies} // 수정: 전체 댓글의 배열을 Reply 컴포넌트로 전달
-            />
+                <Flex align="center" mt={2}>
+                  <Box
+                    as="button"
+                    onClick={onLike}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    {liked ? (
+                      <FaHeart color="red" size={24} />
+                    ) : (
+                      <FaRegHeart color="black" size={24} />
+                    )}
+                    <Text ml="2">{likesCount}</Text>
+                  </Box>
+                </Flex>
+                {allReplies.length > 0 && (
+                  <Box borderWidth="1px" borderRadius="md" p="4" mt="4">
+                    <Flex align="center">
+                      <Image
+                        src={
+                          allReplies[0].creatorPhotoUrl || defaultProfileImage
+                        }
+                        w="32px"
+                        h="32px"
+                        rounded="full"
+                        mr="2"
+                        alt="Profile"
+                      />
+                      <Text fontWeight="bold">{allReplies[0].creatorName}</Text>
+                      <Text ml="2" color="gray.500">
+                        {allReplies[0].creatorEmail}
+                      </Text>
+                    </Flex>
+                    <Text ml="2">{allReplies[0].text}</Text>
+                  </Box>
+                )}
+              </>
+            ) : (
+              <>
+                <Box onClick={togglePopup} cursor="pointer">
+                  <Flex align="center" mb="2">
+                    <Image
+                      src={threadObj.creatorPhotoUrl || defaultProfileImage}
+                      w="32px"
+                      h="32px"
+                      rounded="full"
+                      mr="2"
+                      alt="Profile"
+                    />
+                    <Text fontWeight="bold">{threadObj.creatorName}</Text>
+                    <Text ml="2" color="gray.500">
+                      {threadObj.creatorEmail}
+                    </Text>
+                  </Flex>
+                  <Text fontSize="xl" fontWeight="bold" mb="2">
+                    {threadObj.text}
+                  </Text>
+                  {threadObj.attachmentUrl && (
+                    <Image src={threadObj.attachmentUrl} mb="2" />
+                  )}
+                </Box>
+                {isOwner && (
+                  <Flex>
+                    <Button onClick={onDelete} mr="2">
+                      Delete
+                    </Button>
+                    <Button onClick={toggleEditing}>Edit</Button>
+                  </Flex>
+                )}
+                <Flex align="center" mt={2}>
+                  <Box
+                    as="button"
+                    onClick={onLike}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    {liked ? (
+                      <FaHeart color="red" size={24} />
+                    ) : (
+                      <FaRegHeart color="black" size={24} />
+                    )}
+                    <Text ml="2">{likesCount}</Text>
+                  </Box>
+                </Flex>
+                <Reply
+                  threadId={threadObj.id}
+                  userObj={userObj}
+                  addReply={addReply}
+                  replies={allReplies} // 수정: 전체 댓글의 배열을 Reply 컴포넌트로 전달
+                />
+              </>
+            )}
           </>
         )}
       </Box>
