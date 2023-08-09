9c8633b4ee9efefa8a2ecbe3a8c0de21a0cbf9cf
unknown <121474623+junsuk981102@users.noreply.github.com>
2023-08-09 22:05:30 +0900

    Update screen_Footer.js
    23.08.09 코드수정

--- src/screens/layout/screen_Footer.js
+++ src/screens/layout/screen_Footer.js
@@ -10,30 +10,16 @@ function Footer() {
 
       {/* Footer 전체 화면 배경 */}
       <Flex
-        //정렬
-        flexDirection="row"
         justifyContent="space-between"
-        //여백
         px={boxPadding}
         py="30px"
-        //배경
         bg="white"
         borderTop="1px solid #00A29D"
       >
         {/* Footer 왼쪽 섹션 */}
-        <Flex
-          //정렬
-          flexDirection="column"
-        >
+        <Flex flexDirection="column">
           {/* 로고 사진 */}
-          <Image
-            //사진 위치
-            src="image/main/logo_stot.png"
-            //크기 및 여백
-            w="143px"
-            h="48px"
-            mb="10px"
-          />
+          <Image src="image/main/logo_stot.png" w="143px" h="48px" mb="10px" />
           {/* 기타 정보 */}
           <Text>
             STOT - Security to Token <br />
@@ -42,46 +28,17 @@ function Footer() {
         </Flex>
 
         {/* Footer 오른쪽 섹션 */}
-        <Flex
-          //정렬
-          flexDirection="row"
-          alignItems="center"
-        >
+        <Flex alignItems="center">
           {/* 연락처 정보 */}
-          <Text
-            //여백
-            mr="30px"
-            //기능
-            as="button"
-          >
+          <Text mr="30px" as="button">
             Twitter
           </Text>
-          <Text
-            //여백
-            mr="30px"
-            //기능
-            as="button"
-          >
+          <Text mr="30px" as="button">
             Instagram
           </Text>
-          <Text
-            //여백
-            mr="30px"
-          >
-            Email.
-          </Text>
-          <Text
-            //여백
-            mr="30px"
-          >
-            Tel.
-          </Text>
-          <Text
-            //기능
-            as="button"
-          >
-            이용약관
-          </Text>
+          <Text mr="30px">Email.</Text>
+          <Text mr="30px">Tel.</Text>
+          <Text as="button">이용약관</Text>
         </Flex>
       </Flex>
     </>
@@ -91,3 +48,4 @@ function Footer() {
 export default Footer;
 
 //23.07.21 1차 코드 수정 완료
+//23.08.09 2차 코드 수정 완료
