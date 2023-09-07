bb6bd974c3aca821d25bccd1c656abd935d6420c
kswim57 <kswim8914@gmail.com>
2023-09-07 19:39:32 +0900

    회원가입 에러 수정 & 회원가입 시 기본 자금 지급

--- src/components/user/cpn_Register.jsx
+++ src/components/user/cpn_Register.jsx
@@ -66,6 +66,7 @@ const Register = () => {
         phone: registerPhonenumber,
         birth: registerBirthdate,
         address: registerAddress,
+        funds: 1000000,
       };
       await setDoc(userRef, userData);
 
@@ -94,160 +95,162 @@ const Register = () => {
       </Heading>
       <Divider h="1px" mb="30px" bg="#00A29D" />
 
-      <FormControl
-        justifyContent="center"
-        bg="#E5F2F2"
-        onSubmit={handleFormSubmit}
-      >
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="id">
-            아이디:
-          </FormLabel>
-          <Input
-            type="text"
-            id="id"
-            placeholder="아이디를 입력해주세요"
-            value={registerId}
-            onChange={(event) => {
-              setRegisterId(event.target.value);
-            }}
-            required
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="password">
-            비밀번호:
-          </FormLabel>
-          <Input
-            type="password"
-            id="password"
-            placeholder="비밀번호를 입력해주세요"
-            value={registerPassword}
-            onChange={handlePasswordChange}
-            required
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-        {passwordError && (
-          <Alert textAlign="end" color="red" fontSize="14px">
-            {passwordError}
-          </Alert>
-        )}
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel
+      <form onSubmit={handleFormSubmit}>
+        <FormControl
+          justifyContent="center"
+          bg="#E5F2F2"
+          onSubmit={handleFormSubmit}
+        >
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="id">
+              아이디:
+            </FormLabel>
+            <Input
+              type="text"
+              id="id"
+              placeholder="아이디를 입력해주세요"
+              value={registerId}
+              onChange={(event) => {
+                setRegisterId(event.target.value);
+              }}
+              required
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="password">
+              비밀번호:
+            </FormLabel>
+            <Input
+              type="password"
+              id="password"
+              placeholder="비밀번호를 입력해주세요"
+              value={registerPassword}
+              onChange={handlePasswordChange}
+              required
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+          {passwordError && (
+            <Alert textAlign="end" color="red" fontSize="14px">
+              {passwordError}
+            </Alert>
+          )}
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel
+              fontSize="20px"
+              fontWeight="bold"
+              htmlFor="confirmpassword"
+            >
+              비밀번호 확인:
+            </FormLabel>
+            <Input
+              type="password"
+              id="confirmpassword"
+              placeholder="비밀번호를 한 번 더 입력해주세요"
+              value={registerConfirmpassword}
+              onChange={handleConfirmPasswordChange}
+              required
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+          {confirmPasswordError && (
+            <Alert textAlign="end" color="red" fontSize="14px">
+              {confirmPasswordError}
+            </Alert>
+          )}
+
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="email">
+              이메일:
+            </FormLabel>
+            <Input
+              type="email"
+              id="email"
+              placeholder="예: stot1234@stot.com"
+              value={registerEmail}
+              onChange={(event) => {
+                setRegisterEmail(event.target.value);
+              }}
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="phonenumber">
+              전화번호:
+            </FormLabel>
+            <Input
+              type="tel"
+              id="phonenumber"
+              placeholder="예: 010-1234-5678"
+              value={registerPhonenumber}
+              onChange={(event) => {
+                setRegisterPhonenumber(event.target.value);
+              }}
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="birthDate">
+              생년월일:
+            </FormLabel>
+            <Input
+              type="date"
+              id="birthDate"
+              value={registerBirthdate}
+              onChange={(event) => {
+                setRegisterBirthdate(event.target.value);
+              }}
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+          <FormControl display="flex" justifyContent="space-between" m="15px 0">
+            <FormLabel fontSize="20px" fontWeight="bold" htmlFor="address">
+              주소:
+            </FormLabel>
+            <Input
+              type="text"
+              id="address"
+              placeholder="주소를 입력해주세요"
+              value={registerAddress}
+              onChange={(event) => {
+                setRegisterAddress(event.target.value);
+              }}
+              border="solid 1px #00A29D"
+              borderRadius="xl"
+              maxW="450px"
+            />
+          </FormControl>
+          <Divider h="1px" marginY="30px" bg="#00A29D" />
+          <Button
+            type="submit"
+            maxW="300px"
+            h="75px"
             fontSize="20px"
             fontWeight="bold"
-            htmlFor="confirmpassword"
+            color="white"
+            bg="#00A29D"
+            border="solid 1px #00A29D"
+            borderRadius="xl"
           >
-            비밀번호 확인:
-          </FormLabel>
-          <Input
-            type="password"
-            id="confirmpassword"
-            placeholder="비밀번호를 한 번 더 입력해주세요"
-            value={registerConfirmpassword}
-            onChange={handleConfirmPasswordChange}
-            required
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
+            가입하기
+          </Button>
         </FormControl>
-        {confirmPasswordError && (
-          <Alert textAlign="end" color="red" fontSize="14px">
-            {confirmPasswordError}
-          </Alert>
-        )}
-
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="email">
-            이메일:
-          </FormLabel>
-          <Input
-            type="email"
-            id="email"
-            placeholder="예: stot1234@stot.com"
-            value={registerEmail}
-            onChange={(event) => {
-              setRegisterEmail(event.target.value);
-            }}
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="phonenumber">
-            전화번호:
-          </FormLabel>
-          <Input
-            type="tel"
-            id="phonenumber"
-            placeholder="예: 010-1234-5678"
-            value={registerPhonenumber}
-            onChange={(event) => {
-              setRegisterPhonenumber(event.target.value);
-            }}
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="birthDate">
-            생년월일:
-          </FormLabel>
-          <Input
-            type="date"
-            id="birthDate"
-            value={registerBirthdate}
-            onChange={(event) => {
-              setRegisterBirthdate(event.target.value);
-            }}
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-        <FormControl display="flex" justifyContent="space-between" m="15px 0">
-          <FormLabel fontSize="20px" fontWeight="bold" htmlFor="address">
-            주소:
-          </FormLabel>
-          <Input
-            type="text"
-            id="address"
-            placeholder="주소를 입력해주세요"
-            value={registerAddress}
-            onChange={(event) => {
-              setRegisterAddress(event.target.value);
-            }}
-            border="solid 1px #00A29D"
-            borderRadius="xl"
-            maxW="450px"
-          />
-        </FormControl>
-        <Divider h="1px" marginY="30px" bg="#00A29D" />
-        <Button
-          type="submit"
-          maxW="300px"
-          h="75px"
-          fontSize="20px"
-          fontWeight="bold"
-          color="white"
-          bg="#00A29D"
-          border="solid 1px #00A29D"
-          borderRadius="xl"
-        >
-          가입하기
-        </Button>
-      </FormControl>
+      </form>
     </Flex>
   );
 };
