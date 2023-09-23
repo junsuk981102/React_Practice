c5a54937892168213cfa25f82f5631ed8417667d
kswim57 <kswim8914@gmail.com>
2023-09-23 19:10:46 +0900

    커뮤니티 생성 화면 UI 수정, 로컬 이미지 업로드 기능 추가

--- src/screens/community/screen_Community_Make.js
+++ src/screens/community/screen_Community_Make.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { dbService } from "../../firebase-config";
+import { dbService, storageService } from "../../firebase-config";
 import { useNavigate } from "react-router-dom";
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 import {
   FormControl,
   FormLabel,
@@ -9,7 +11,11 @@ import {
   Button,
   Heading,
   Text,
+  Box,
+  Image,
+  Grid,
 } from "@chakra-ui/react";
+import { FiImage } from "react-icons/fi";
 
 const ScreenRoomMake = () => {
   const navi = useNavigate();
@@ -63,6 +69,29 @@ const ScreenRoomMake = () => {
     }));
   };
 
+  //사진을 추가했을 때 동작하는 이벤트 핸들러
+  const onFileChange = (e) => {
+    const {
+      target: { files },
+    } = e;
+    const commFile = files[0];
+
+    // 파일이 선택되지 않았을 경우에 처리.
+    if (!commFile) {
+      setAttachment("");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (finishedEvent) => {
+      const {
+        target: { result },
+      } = finishedEvent;
+      setAttachment(result || "");
+    };
+    reader.readAsDataURL(commFile);
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCommunityData((prevData) => ({
@@ -71,23 +100,54 @@ const ScreenRoomMake = () => {
     }));
   };
 
+  //Post 전 사진을 삭제할떄 동작
+  const clearAttachment = () => {
+    setAttachment("");
+  };
+
   function handleClick(text) {
     navi(`${text}`);
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    let attachmentUrl = "";
+
+    //첨부 파일이 있는 경우에만 처리
+    if (attachment !== "") {
+      const attachmentRef = ref(
+        storageService,
+        `community/${communityData.name}`
+      );
+      const response = await uploadString(
+        attachmentRef,
+        attachment,
+        "data_url"
+      );
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
+
     await dbService.collection("community_list").add({
       com_name: communityData.name,
       com_category: communityData.categories,
       com_info: communityData.info,
-      com_profileImg: communityData.image,
+      com_profileImg: attachmentUrl,
       com_total_investment: communityData.investmentAmount,
       com_now_investment: 0,
       com_ticket_price: communityData.ticketPrice,
       com_ticket_max: communityData.maxTicket,
+      com_total_ticket:
+        communityData.investmentAmount / communityData.ticketPrice,
       com_createAt: Date.now(),
-      com_member: 1,
+      com_member: 0,
+      com_fall: 0,
+      com_fone: 0,
+      com_fthree: 0,
+      com_ftwo: 0,
+      com_sall: 0,
+      com_sno: 0,
+      com_syes: 0,
     });
     setCommunityData({
       name: "",
@@ -140,7 +200,7 @@ const ScreenRoomMake = () => {
             bg="white"
           >
             {/* 기본 정보 섹션 */}
-            <Heading mb="10px" size="md">
+            <Heading mb="30px" size="md">
               기본 정보
             </Heading>
             <Flex flexDirection="column" px="20px">
@@ -170,11 +230,13 @@ const ScreenRoomMake = () => {
 
               <FormControl mb="15px">
                 <FormLabel>커뮤니티 카테고리</FormLabel>
-                <Flex>
+                <Grid
+                  templateColumns="repeat(auto-fill, minmax(100px, 1fr))"
+                  gap={2}
+                >
                   {categories.map((category) => (
-                    //카테고리 선택 버튼
                     <Button
-                      m="2px 3px"
+                      key={category}
                       colorScheme="teal"
                       variant={
                         selectedCategories.includes(category)
@@ -182,26 +244,58 @@ const ScreenRoomMake = () => {
                           : "outline"
                       }
                       size="sm"
-                      key={category}
                       onClick={() => handleCategoryToggle(category)}
                     >
                       {category}
                     </Button>
                   ))}
-                </Flex>
+                </Grid>
               </FormControl>
 
               <FormControl mb="15px">
                 <FormLabel>커뮤니티 대표 이미지</FormLabel>
                 <Flex>
                   <Input
-                    type="text"
+                    type="file"
+                    accept="image/*"
+                    display="none"
                     name="image"
-                    value={communityData.image}
-                    onChange={handleChange}
+                    // value={communityData.image}
+                    onChange={onFileChange}
                     placeholder="이미지의 URL을 넣으세요."
+                    id="fileInput"
                     required
                   />
+                  <label htmlFor="fileInput">
+                    <Button
+                      as="span"
+                      variant="outline"
+                      size="md"
+                      colorScheme="teal"
+                      leftIcon={<FiImage />}
+                    >
+                      이미지 추가
+                    </Button>
+                  </label>
+                  {/* 첨부 파일이 있는 경우 미리보기 및 삭제 버튼 표시 */}
+                  {attachment && (
+                    <Box ml="10px">
+                      <Text
+                        color="teal"
+                        fontWeight="bold"
+                        cursor="pointer"
+                        onClick={clearAttachment}
+                      >
+                        첨부파일 삭제
+                      </Text>
+                      <Image
+                        src={attachment}
+                        alt="attachment"
+                        w="100px"
+                        mt="5px"
+                      />
+                    </Box>
+                  )}
                 </Flex>
               </FormControl>
             </Flex>
@@ -211,71 +305,64 @@ const ScreenRoomMake = () => {
               투자 정보
             </Heading>
             <Flex flexDirection="column" px="20px">
-              <FormControl mb="15px" textAlign="end">
-                <Flex alignItems="center" justifyContent="space-between">
-                  <FormLabel>관심있는 회사:</FormLabel>
-                  <Input
-                    maxW="700px"
-                    type="text"
-                    name="company"
-                    value={communityData.company}
-                    onChange={handleChange}
-                    placeholder="관심있는 회사의 이름을 입력하세요."
-                    required
-                  />
-                </Flex>
+              <FormControl mb="15px">
+                <FormLabel>관심있는 회사</FormLabel>
+                <Input
+                  maxW="700px"
+                  type="text"
+                  name="company"
+                  value={communityData.company}
+                  onChange={handleChange}
+                  placeholder="관심있는 회사의 이름을 입력하세요."
+                  required
+                />
               </FormControl>
 
-              <FormControl mb="15px" textAlign="end">
-                <Flex alignItems="center" justifyContent="space-between">
-                  <Text>투자 목표 금액:</Text>
-                  <Input
-                    maxW="700px"
-                    type="text"
-                    name="investmentAmount"
-                    value={communityData.investmentAmount}
-                    onChange={handleChange}
-                    placeholder="커뮤니티의 최종 투자 목표 금액을 입력하세요."
-                    required
-                  />
-                </Flex>
+              <FormControl mb="15px">
+                <FormLabel>투자 목표 금액</FormLabel>
+                <Input
+                  maxW="700px"
+                  type="text"
+                  name="investmentAmount"
+                  value={communityData.investmentAmount}
+                  onChange={handleChange}
+                  placeholder="커뮤니티의 최종 투자 목표 금액을 입력하세요."
+                  required
+                />
                 <Text mt="8px">
                   {convertCurrency(communityData.investmentAmount)}
                 </Text>
               </FormControl>
 
-              <FormControl mb="15px" textAlign="end">
-                <Flex alignItems="center" justifyContent="space-between">
-                  <Text>티켓 가격:</Text>
-                  <Input
-                    maxW="700px"
-                    name="ticketPrice"
-                    value={communityData.ticketPrice}
-                    onChange={handleChange}
-                    placeholder="티켓의 가격을 입력하세요."
-                    required
-                  ></Input>
-                </Flex>
+              <FormControl mb="15px">
+                <FormLabel>티켓 가격</FormLabel>
+                <Input
+                  maxW="700px"
+                  name="ticketPrice"
+                  value={communityData.ticketPrice}
+                  onChange={handleChange}
+                  placeholder="티켓의 가격을 입력하세요."
+                  required
+                />
                 <Text mt="8px">
                   {convertCurrency(communityData.ticketPrice)}
                 </Text>
               </FormControl>
 
-              <FormControl mb="15px" textAlign="end">
-                <Flex alignItems="center" justifyContent="space-between">
-                  <Text>1인당 최대 구매 개수: </Text>
-                  <Input
-                    maxW="700px"
-                    name="maxTicket"
-                    value={communityData.maxTicket}
-                    onChange={handleChange}
-                    placeholder="티켓의 1인당 최대 구매 개수를 입력하세요."
-                    required
-                  ></Input>
-                </Flex>
+              <FormControl mb="15px">
+                <FormLabel>1인당 최대 구매 개수</FormLabel>
+                <Input
+                  maxW="700px"
+                  name="maxTicket"
+                  value={communityData.maxTicket}
+                  onChange={handleChange}
+                  placeholder="티켓의 1인당 최대 구매 개수를 입력하세요."
+                  required
+                />
               </FormControl>
             </Flex>
-            <Flex justifyContent="center">
+
+            <Flex justifyContent="center" mt="20px">
               <Button maxW="250px" h="50px" colorScheme="teal" type="submit">
                 커뮤니티 생성
               </Button>
